e3300c81b555f4cf6017d70a791147e7720cbdb8
Ramtin Khoee <rahmteen@heds.io>
2022-08-29 17:42:35 -0700

    boilerplate w/ state mgmt

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import { Landing } from './Landing';
+export default (): JSX.Element => <Landing />;
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,tsx,ts}'],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      sans: ['Barlow', 'sans-serif'],
+      serif: ['DM Mono', 'monospace'],
+    },
+    extend: {
+      gridTemplateColumns: {
+        24: 'repeat(24, minmax(0, 1fr))',
+      },
+      height: {
+        0.25: '0.075rem',
+      },
+      margin: {
+        0.25: '0.075rem',
+      },
+      padding: {
+        0.25: '0.075rem',
+      },
+      colors: {
+        neutral: {
+          850: '#212121',
+          950: '#141414',
+          975: '#101010',
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('flowbite/plugin')],
+};
