d17f312239788279045a432398b1b139450bdf0b
Ramtin Khoee <rahmteen@heds.io>
2022-08-29 22:41:34 -0700

    updated folder structure and module resolvers

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,8 +1,8 @@
+import React, { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
-import { Bars3BottomLeftIcon, BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { Bars3BottomLeftIcon, BellIcon } from '@heroicons/react/24/outline';
 import { useAccount, useConnect, useDisconnect } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
-import React, { Fragment } from 'react';
 import { useDispatch } from 'react-redux';
 import { Dispatch } from 'src/store';
 import { classNames } from '../../utils/classNames';
@@ -24,55 +24,21 @@ export const Navigation = () => {
     <div className="flex flex-col md:pl-64">
       <div className="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white shadow">
         <button
-          type="button"
           className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
           onClick={() => dispatch.layoutModel.setSidebarOpen(true)}
         >
           <span className="sr-only">Open sidebar</span>
           <Bars3BottomLeftIcon className="h-6 w-6" aria-hidden="true" />
         </button>
-        <div className="flex flex-1 justify-between px-4">
-          <div className="flex flex-1">
-            <form className="flex w-full md:ml-0" action="#" method="GET">
-              <label htmlFor="search-field" className="sr-only">
-                Search
-              </label>
-              <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
-                  <MagnifyingGlassIcon className="h-5 w-5" aria-hidden="true" />
-                </div>
-                <input
-                  id="search-field"
-                  className="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-sm"
-                  placeholder="Search"
-                  type="search"
-                  name="search"
-                />
-              </div>
-            </form>
-          </div>
+        <div className="flex flex-1 justify-end px-4">
           <div className="ml-4 flex items-center md:ml-6">
-            <button
-              type="button"
-              className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            >
+            <button className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
               <span className="sr-only">View notifications</span>
               <BellIcon className="h-6 w-6" aria-hidden="true" />
             </button>
-
-            {/* Profile dropdown */}
             <Menu as="div" className="relative ml-3">
               <div>
-                <Menu.Button className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                  {isConnected ? (
-                    <div>
-                      Connected to {address}
-                      <button onClick={() => disconnect()}>Disconnect</button>
-                    </div>
-                  ) : (
-                    <button onClick={() => connect()}>Connect Wallet</button>
-                  )}
-                </Menu.Button>
+                <Menu.Button className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"></Menu.Button>
               </div>
               <Transition
                 as={Fragment}
