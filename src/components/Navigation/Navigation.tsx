febb62c24f4be57d57febca25648c4647f8789cb
Ramtin Khoee <rahmteen@heds.io>
2022-08-31 22:31:23 -0700

    added neutral styling to sidebar

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -20,34 +20,14 @@ export const Navigation = () => {
   const dispatch = useDispatch<Dispatch>();
   return (
     <div className="flex flex-col md:pl-52">
-      <div className="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-gray-300 shadow">
+      <div className="sticky top-0 z-10 flex h-16 flex-shrink-0">
         <button
-          className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
+          className="border-r border-gray-400 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
           onClick={() => dispatch.layoutModel.setSidebarOpen(true)}
         >
           <Bars3BottomLeftIcon className="h-6 w-6" aria-hidden="true" />
         </button>
         <div className="flex flex-1 justify-end gap-x-2 items-center px-2">
-          {/* <div className="flex flex-1">
-            <form className="flex w-full md:ml-0" action="#" method="GET">
-              <label htmlFor="search-field" className="sr-only">
-                Search
-              </label>
-              <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
-                  <MagnifyingGlassIcon className="h-5 w-5" aria-hidden="true" />
-                </div>
-                <input
-                  autoComplete="off"
-                  id="search-field"
-                  className="block h-full w-full border-transparent py-2 pl-8 pr-3 bg-gray-300 text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-sm"
-                  placeholder="Search"
-                  type="search"
-                  name="search"
-                />
-              </div>
-            </form>
-          </div> */}
           {isConnected && profileData ? <Avatar profileData={profileData} /> : <ConnectButton />}
           <Menu as="div" className="relative ml-3">
             <div>
--- src/components/SidebarDesktop/SidebarDesktop.tsx
+++ src/components/SidebarDesktop/SidebarDesktop.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { CalendarIcon, ChartBarIcon, FolderIcon, HomeIcon, InboxIcon, UsersIcon } from '@heroicons/react/24/solid';
+import { BookOpenIcon, CalendarIcon, GlobeAltIcon, MusicalNoteIcon, UsersIcon } from '@heroicons/react/24/solid';
 import { classNames } from '../../utils/classNames';
 import { Link } from 'react-router-dom';
 
 const navigation = [
-  { name: 'Explore', href: '/explore', icon: HomeIcon, current: true },
-  { name: 'Tapes', href: '/tapes', icon: UsersIcon, current: false },
-  { name: 'Artists', href: '/artists', icon: FolderIcon, current: false },
+  { name: 'Explore', href: '/explore', icon: GlobeAltIcon, current: true },
+  { name: 'Tapes', href: '/tapes', icon: MusicalNoteIcon, current: false },
+  { name: 'Artists', href: '/artists', icon: UsersIcon, current: false },
   { name: 'Vote', href: '/vote', icon: CalendarIcon, current: false },
   // { name: 'Spaces', href: '/spaces', icon: InboxIcon, current: false },
-  { name: 'About', href: '/about', icon: ChartBarIcon, current: false },
+  { name: 'About', href: '/about', icon: BookOpenIcon, current: false },
 ];
 
 export const SidebarDesktop = () => {
@@ -19,38 +19,45 @@ export const SidebarDesktop = () => {
   return (
     <div>
       <div className="hidden md:fixed md:inset-y-0 md:flex md:w-52 md:flex-col shadow-sm">
-        <div className="flex min-h-0 flex-1 flex-col bg-gray-300">
-          <div className="flex h-16 flex-shrink-0 items-center bg-gray-300 px-4">
+        <div className="flex min-h-0 flex-1 flex-col bg-gray-100">
+          <div className="flex h-16 flex-shrink-0 justify-between items-center bg-gray-100 px-5 mb-1 border-b border-gray-400">
             <img
-              className="h-10 w-auto invert"
+              className="h-8 w-auto invert"
               src="https://firebasestorage.googleapis.com/v0/b/heds-34ac0.appspot.com/o/public%2Fheddot.png?alt=media&token=a1e1d838-5332-4ab6-bb1e-159846efbfd4"
               alt="heds"
             />
+            <h1 className="text-neutral-800 font-medium tracking-widest text-lg">heds</h1>
           </div>
           <div className="flex flex-1 flex-col overflow-y-auto">
-            <nav className="flex-1 space-y-1 px-2 py-2">
+            <nav className="flex-1 space-y-2 px-2 py-1">
               {navigation.map((item) => (
                 <Link
                   key={item.name}
                   to={item.href}
                   className={classNames(
                     "/" + item.name.toLowerCase() === pathname
-                      ? 'bg-red-400 text-white'
-                      : 'text-gray-400 hover:bg-gray-700 hover:text-white',
-                    'group flex items-center px-3 py-2 text-sm font-regular font-regular tracking-widest rounded-md shadow-sm transition-all',
+                      ? 'bg-neutral-700 text-white'
+                      : 'bg-neutral-700 text-white hover:bg-gray-700 hover:text-white',
+                    'group flex items-center justify-start px-4 py-3 text-xs font-normal font-regular tracking-widest rounded-sm shadow-sm transition-all',
                   )}
                 >
-                  {/* <item.icon
+                  <item.icon
                     className={classNames(
-                      item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300',
-                      'mr-3 flex-shrink-0 h-6 w-6',
+                      item.current ? 'text-gray-100' : 'text-gray-100 group-hover:text-gray-300',
+                      'mr-3 flex-shrink-0 h-4 w-4',
                     )}
                     aria-hidden="true"
-                  /> */}
+                  />
                   {item.name}
                 </Link>
               ))}
             </nav>
+            <div className="flex justify-center gap-x-2 py-5">
+              <span className='text-sm font-thin tracking-wide text-indigo-600'>discord</span>
+               <span className='text-sm font-thin'>|</span>
+              <span className='text-sm font-thin tracking-wide text-blue-600'>twitter</span>
+             
+            </div>
           </div>
         </div>
       </div>
