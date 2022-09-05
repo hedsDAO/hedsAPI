526d08c4d9b855260809e7ebc3cb2cfbe2959546
Ramtin Khoee <rahmteen@heds.io>
2022-09-05 10:46:34 -0700

    added pagination to artists page

--- src/components/Artists/PaginationBar/index.tsx
+++ src/components/Artists/PaginationBar/index.tsx
@@ -1,3 +1,3 @@
 import React from 'react';
 import { PaginationBar } from './PaginationBar';
-export default ({total, perPage}: {total: number; perPage: number}): JSX.Element => <PaginationBar total={total} perPage={perPage} />;
+export default (): JSX.Element => <PaginationBar />;
