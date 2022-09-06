cdc3b520dd76cab721f4f148c3b61907a9b22ca6
Ramtin Khoee <rahmteen@heds.io>
2022-09-05 18:54:25 -0700

    updated styling for pagination and artist page

--- src/components/Artists/PaginationBar/PaginationBar.tsx
+++ src/components/Artists/PaginationBar/PaginationBar.tsx
@@ -11,11 +11,11 @@ export const PaginationBar = () => {
     <nav className="flex items-center justify-between px-4 sm:px-6 lg:max-w-7xl lg:px-4 max-w-7xl mx-auto">
       <button
         onClick={() => dispatch.artistModel.setPreviousPage()}
-        className="flex justify-start mr-auto px-3 py-1 bg-neutral-950 hover:bg-neutral-800 rounded-full transition-all"
+        className="flex justify-start mr-auto px-3 py-1 bg-neutral-950 hover:bg-neutral-800 rounded-full transition-all shadow-sm"
       >
         <ArrowLongLeftIcon className="h-6 w-6 text-gray-200" aria-hidden="true" />
       </button>
-      <div className="hidden md:-mt-px md:flex gap-x-1">
+      <div className="hidden md:-mt-px md:flex gap-x-2">
         {[...Array((Math.ceil(artistData.totalArtists / 10) * 10) / 10)].map((index, i) => {
           return (
             <button
@@ -23,7 +23,7 @@ export const PaginationBar = () => {
               onClick={() => dispatch.artistModel.setCurrentPage(i + 1)}
               className={classNames(
                 i + 1 === artistData.currentPage
-                  ? 'bg-neutral-950'
+                  ? 'bg-neutral-950 shadow-sm'
                   : 'text-neutral-800 hover:bg-neutral-950 hover:text-gray-200',
                 'inline-flex items-center rounded-full px-3 py-1 font-medium text-gray-200 transition-all',
               )}
@@ -35,7 +35,7 @@ export const PaginationBar = () => {
       </div>
       <button
         onClick={() => dispatch.artistModel.setNextPage()}
-        className="flex justify-end ml-auto px-3 py-1 bg-neutral-950 hover:bg-neutral-800 rounded-full transition-all"
+        className="flex justify-end ml-auto px-3 py-1 bg-neutral-950 hover:bg-neutral-800 rounded-full transition-all shadow-sm"
       >
         <ArrowLongRightIcon className="h-6 w-6 text-gray-200" aria-hidden="true" />
       </button>
