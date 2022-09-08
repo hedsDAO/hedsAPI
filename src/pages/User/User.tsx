6cf1b3e3e49e8640d1f5258ac83a7c0339898847
Ramtin Khoee <rahmteen@heds.io>
2022-09-07 21:04:01 -0700

    removed badges

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -19,16 +19,13 @@ export const User = () => {
       {userData && (
         <div className="bg-white">
           <div className="mx-auto py-10 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
-            <div className="lg:grid lg:grid-cols-8 lg:grid-rows-1 lg:gap-x-6 lg:gap-y-10 xl:gap-x-10 lg:max-w-7xl">
+            <div className="lg:grid lg:grid-cols-7 lg:grid-rows-1 lg:gap-x-6 lg:gap-y-10 xl:gap-x-10 lg:max-w-7xl">
               <div className="lg:col-span-2 lg:row-end-1">
                 <img
                   src={userData.profilePicture}
                   alt={userData.displayName}
                   className="object-fill w-full h-full aspect-square rounded-lg object-center"
                 />
-                {/* <span className="flex mx-auto items-center rounded-lg lg:-space-x-1 text-xs bg-red-100 text-neutral-800 border border-gray-900 px-2 py-0.5 tracking-tight shadow-md font-semibold">
-                  <i className="fa-sharp fa-solid fa-waveform-lines mr-1"></i> artist
-                </span> */}
               </div>
               <div className="mx-auto max-w-2xl sm:mt-10 lg:col-span-5 lg:row-span-2 lg:row-end-2 lg:mt-0 lg:max-w-none w-full px-1">
                 <div className="flex flex-col">
@@ -37,7 +34,7 @@ export const User = () => {
                       {userData.displayName}
                     </h1>
                   </div>
-                  <p className="text-gray-500 text-sm">{userData.description || '...'}</p>
+                  <p className="text-gray-500 text-sm -mb-1 px-1">{userData.description || '...'}</p>
                 </div>
                 <div className="inline-flex items-center justify-start gap-x-1">
                   {userData?.twitterHandle && (
@@ -55,16 +52,6 @@ export const User = () => {
                         userData?.wallet.slice(userData?.wallet?.length - 4, userData?.wallet?.length)}
                     </span>
                   )}
-                </div>
-
-                <p className="text-gray-500 text-sm font-semibold tracking-tight mt-5 px-1">badges:</p>
-                <div className="grid grid-cols-4 lg:grid-cols-8 gap-x-2 mt-2">
-                  {userData?.badges.map((badge) => (
-                    <div key={badge.name} className="flex flex-col items-center text-black col-span-1 bg-gray-100 rounded-lg p-1.5 shadow-sm border border-gray-200 gap-y-1">
-                      <img className="rounded-md shadow-sm" src={badge.image} />
-                      <span className="text-gray-400 text-xs font-medium tracking-wide px-1 uppercase py-1">{badge.name}</span>
-                    </div>
-                  ))}
                 </div>
               </div>
             </div>
