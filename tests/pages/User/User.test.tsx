dea7330a7bdee2d0d58887f0f7ffcdf897308494
Ramtin Khoee <rahmteen@heds.io>
2022-09-24 18:55:28 -0700

    added user tests

--- tests/pages/User/User.test.tsx
+++ tests/pages/User/User.test.tsx
@@ -1,13 +1,52 @@
 import React from 'react';
 import { User } from '@pages/User/User';
-import renderer from 'react-test-renderer';
 import { store } from '@/store';
 import { renderWithRematchStore } from '../../utils/testUtils';
-import { screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
+import { userData } from '../../mockData/User/UserData';
+import { formatWallet } from '@utils/formatWallet';
 
-describe('Explore Unit', () => {
-  test('should render component properly', () => {
-    renderWithRematchStore(<User />, store);
-    expect(screen.getByText('hello world')).toBeInTheDocument();
+describe('User unit', () => {
+  beforeEach(
+    async () =>
+      await act(() => {
+        store.dispatch.userModel.setUserData(userData);
+        renderWithRematchStore(<User />, store);
+      }),
+  );
+  it('should render profile picture', () => {
+    const profilePictureImage = screen.getByTestId('user-profile-picture');
+    const { profilePicture } = userData;
+    expect(profilePictureImage).toHaveAttribute('src', profilePicture);
+  });
+  it('should render display name', () => {
+    const displayNameText = screen.getByTestId('user-display-name');
+    const { displayName } = userData;
+    expect(displayNameText).toHaveTextContent(displayName);
+  });
+  it('should render desciption', () => {
+    const descriptionText = screen.getByTestId('user-description');
+    const { description } = userData;
+    expect(descriptionText).toHaveTextContent(description || '...');
+  });
+  it('should render twitter link if present', () => {
+    const twitterLink = screen.queryByTestId('user-twitter-link');
+    const twitterText = screen.queryByTestId('user-twitter-text');
+    const { twitterHandle } = userData;
+    if (twitterHandle) {
+      expect(twitterLink).toHaveAttribute('href', `https://www.twitter.com/${twitterHandle}`);
+      expect(twitterText).toHaveTextContent(`@${twitterHandle}`);
+    } else expect(twitterText && twitterLink).toBeFalsy();
+  });
+  it('should render wallet address', () => {
+    const walletText = screen.queryByTestId('user-wallet');
+    const { wallet } = userData;
+    expect(walletText).toHaveTextContent(formatWallet(wallet));
+  });
+  it('should render user submissions', () => {
+    const userSubmissions = screen.queryByTestId('user-submissions');
+    const { submissions } = userData;
+    if (!submissions) expect(userSubmissions).toBeEmptyDOMElement();
+    else Object.values(submissions.heds.hedstape).map((sub) => expect(userSubmissions).toHaveTextContent(sub.track));
   });
 });
