229c151652f1557b37e7f35e262d60cafb1b3acf
Ramtin Khoee <rahmteen@heds.io>
2022-09-26 13:04:27 -0700

    update file paths

--- src/common/constants/User.js
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_PROFILE_PICTURE =
-  'https://firebasestorage.googleapis.com/v0/b/heds-34ac0.appspot.com/o/users%2F0x000000000000000000000000000000.png?alt=media&token=55cb53fe-736d-4b1e-bcd0-bf17bc7146dc';
