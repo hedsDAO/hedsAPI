8e8e0b572fdda95f1c7e9681680eb35a4c11e79c
Ramtin Khoee <rahmteen@heds.io>
2022-09-26 19:13:54 -0700

    nits + userwrapper for user page

--- src/modules/user/components/ProfilePicture.tsx
+++ src/modules/user/components/ProfilePicture.tsx
@@ -3,7 +3,7 @@ import { User } from '@/models/common';
 import { Skeleton } from '@chakra-ui/react';
 import DEFAULT_PROFILE_PICTURE from '/public/default.png';
 
-export const ProfilePicture = ({ loading, userData }: { loading: boolean; userData: User }) => {
+const ProfilePicture = ({ loading, userData }: { loading: boolean; userData: User }) => {
   return (
     <Skeleton
       className="lg:min-h-[18rem] lg:max-h-[18rem] lg:min-w-[18rem] lg:max-w-[18rem]"
@@ -21,3 +21,5 @@ export const ProfilePicture = ({ loading, userData }: { loading: boolean; userDa
     </Skeleton>
   );
 };
+
+export default ProfilePicture;
---
+++ src/modules/user/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as CopyWalletButton } from './CopyWalletButton';
+export { default as ProfilePicture } from './ProfilePicture';
+export { default as TwitterLinkButton } from './TwitterLinkButton';
+export { default as UserSubmissions } from './UserSubmissions';
