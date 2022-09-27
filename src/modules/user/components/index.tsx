8789482d509a58e9fa57bc1e0c48d74f026d88e0
Ramtin Khoee <rahmteen@heds.io>
2022-09-27 11:18:21 -0700

    update file structure

--- src/modules/user/components/index.tsx
+++ src/modules/user/components/index.tsx
@@ -1,6 +1,6 @@
-export { default as CopyWalletButton } from './CopyWalletButton';
-export { default as ProfilePicture } from './ProfilePicture';
-export { default as TwitterLinkButton } from './TwitterLinkButton';
-export { default as UserSubmissions } from './UserSubmissions';
-export { default as DisplayName } from './DisplayName';
-export { default as UserDescription } from './UserDescription';
+export { default as CopyWalletButton } from './CopyWalletButton/CopyWalletButton';
+export { default as ProfilePicture } from './ProfilePicture/ProfilePicture';
+export { default as TwitterLinkButton } from './TwitterLinkButton/TwitterLinkButton';
+export { default as UserSubmissions } from './UserSubmissions/UserSubmissions';
+export { default as DisplayName } from './DisplayName/DisplayName';
+export { default as UserDescription } from './UserDescription/UserDescription';
