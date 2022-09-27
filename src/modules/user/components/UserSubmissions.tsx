9ff8364e1b759d15d0b69980135409a1f36ea865
Ramtin Khoee <rahmteen@heds.io>
2022-09-27 10:50:00 -0700

    updated description value and tests

--- src/modules/user/components/UserSubmissions.tsx
+++ src/modules/user/components/UserSubmissions.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import { User } from '@/models/common';
 import { formatTime } from '@/utils';
+import { Stack, Skeleton } from '@chakra-ui/react';
 
-const UserSubmissions = ({ userData }: { userData: User }) => {
+const UserSubmissions = ({ loading, userData }: { loading: boolean; userData: User }) => {
   return (
     <ul data-testid="user-submissions" role="list" className="divide-y divide-gray-200">
-      {userData?.submissions?.heds?.hedstape  &&
-        Object.entries(userData.submissions.heds.hedstape).map(([id, submission], i) => (
-          <li className="text-xs text-gray-600" key={i}>
-            <a href="#" className="block hover:bg-gray-50">
-              <div className="flex items-center px-2 py-3 gap-x-2">
-                <span className="text-xs font-thin">HT{id}</span>
-                <div className="min-w-0 flex-1 sm:flex">{submission.track}</div>
-                <div className="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
-                  <span className="mr-2">{formatTime(submission.duration)}</span>
-                  <i className="fa-solid fa-play"></i>
-                </div>
-              </div>
-            </a>
-          </li>
-        ))}
+      <Stack spacing="2">
+        <Skeleton height="4" fadeDuration={2} isLoaded={!loading}>
+          {userData?.submissions?.heds?.hedstape &&
+            Object.entries(userData.submissions.heds.hedstape).map(([id, submission], i) => (
+              <li className="text-xs text-gray-600" key={i}>
+                <a href="#" className="block hover:bg-gray-50">
+                  <div className="flex items-center gap-x-2">
+                    <span className="text-xs font-thin">HT{id}</span>
+                    <div className="min-w-0 flex-1 sm:flex">{submission.track}</div>
+                    <div className="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
+                      <span className="mr-2">{formatTime(submission.duration)}</span>
+                      <i className="fa-solid fa-play"></i>
+                    </div>
+                  </div>
+                </a>
+              </li>
+            ))}
+        </Skeleton>
+      </Stack>
     </ul>
   );
 };
