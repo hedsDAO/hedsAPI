b3c72d1e4b89bbeee35359d6149aa71eaf4e898d
Ramtin Khoee <rahmteen@heds.io>
2022-09-28 10:27:49 -0700

    updated display name and refresh user state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
--- src/common/components/wrappers/ProfileWrapper/ProfileWrapper.tsx
+++ src/common/components/wrappers/ProfileWrapper/ProfileWrapper.tsx
@@ -1,14 +1,30 @@
 import React, { Fragment } from 'react';
+import { useNavigate, useLocation } from 'react-router';
 import { useAccount } from 'wagmi';
 import { Dispatch } from 'src/store';
 import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 export const ProfileWrapper = ({ children }: { children: React.ReactNode }) => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
   const dispatch = useDispatch<Dispatch>();
+  const { address } = useAccount();
   useAccount({
     onConnect({ address }) {
       dispatch.profileModel.getProfileData(address.toLowerCase());
     },
+    onDisconnect() {
+      console.log('hello');
+      navigate('/');
+    },
   });
+  useEffect(() => {
+    if (!address && pathname === '/profile') navigate('/');
+    if (address && pathname === '/profile') dispatch.profileModel.getProfileData(address.toLowerCase());
+    return () => {
+      
+    }
+  }, [pathname]);
   return <Fragment>{children}</Fragment>;
 };
