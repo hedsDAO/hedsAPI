cc0b2bf9b2dfb15ca23e16e77d1dda945f6bbabd
Ramtin Khoee <rahmteen@heds.io>
2022-09-29 16:52:57 -0700

    updated fetching

--- src/modules/profile/components/ProfileCollection/ProfileCollection.tsx
+++ src/modules/profile/components/ProfileCollection/ProfileCollection.tsx
@@ -5,16 +5,14 @@ import { User, UserCollection } from '@/modules/profile/models/common';
 import useTapeOwnership from '@/hooks/useTapeOwnership';
 import { Dispatch, store } from '@/store';
 import { useDispatch } from 'react-redux';
-import { isEmpty } from '@/utils';
 
 const ProfileCollection = ({ profileData, loading }: { profileData: User; loading: boolean }) => {
   const dispatch = useDispatch<Dispatch>();
   const tapeDataForOwnership: UserCollection = store.select.tapesModel.getTapeDataForOwnership(store.getState());
-  const { data, refetch, isRefetching } = useTapeOwnership(profileData?.wallet || '', !isEmpty(profileData?.collection) ? {} : tapeDataForOwnership);
+  const { data, refetch, isRefetching } = useTapeOwnership(profileData?.wallet || '', profileData?.collection ? {} : tapeDataForOwnership);
+
   useEffect(() => {
-    if ((data?.length, tapeDataForOwnership, profileData?.wallet)) {
-      dispatch.profileModel.updateUserCollection([profileData.wallet, tapeDataForOwnership, data]);
-    }
+    if ((data?.length, tapeDataForOwnership, profileData?.wallet)) dispatch.profileModel.updateUserCollection([profileData.wallet, tapeDataForOwnership, data]);
   }, [data, isRefetching]);
 
   return (
--- src/utils/isEmpty.ts
+++ src/utils/isEmpty.ts
@@ -1,8 +1,8 @@
 function isEmpty(obj: any) {
+  if (typeof obj !== 'object') return true;
   for (const prop in obj) {
     if (obj.hasOwnProperty(prop)) return false;
   }
-
   return true;
 }
 
