bd8107f548e03a9d7d6e3b0e5a91c37191794e09
Ramtin Khoee <rahmteen@heds.io>
2022-10-04 12:46:56 -0700

    twitter loaders and screen update

--- src/modules/profile/modals/TwitterModal/TwitterModal.tsx
+++ src/modules/profile/modals/TwitterModal/TwitterModal.tsx
@@ -1,40 +1,25 @@
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
 import { ModalContainer } from '@/common/components/containers/ModalContainer/ModalContainer';
 import { Dispatch, RootState } from '@/store';
-import { Dialog } from '@headlessui/react';
+import { Dialog, Transition } from '@headlessui/react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  Avatar,
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Stack,
-  StackDivider,
-  Switch,
-  Tag,
-  TagLabel,
-  Text,
-} from '@chakra-ui/react';
-import { IconAlertCircle, IconClipboard, IconKey, IconLink } from '@tabler/icons';
+import { Avatar, Button, Flex, FormControl, FormLabel, Input, InputGroup, InputRightElement, Stack, StackDivider, Tag, TagLabel, Text } from '@chakra-ui/react';
+import { IconAlertCircle, IconCheck, IconClipboard, IconKey, IconLink } from '@tabler/icons';
 import { formatWallet } from '@/utils';
+import { TwitterStep } from '../../models/twitterModalModel';
 
 export const TwitterModal = () => {
   const dispatch = useDispatch<Dispatch>();
   const { isOpen } = useSelector((state: RootState) => state.modalModel);
-  const twitterModalData = useSelector((state: RootState) => state.twitterModalModel);
+  const { currentStep, loading, hashedTweet, copied, windowParams, pastedTweetUrl } = useSelector((state: RootState) => state.twitterModalModel);
   const profileData = useSelector((state: RootState) => state.profileModel);
-  console.log(twitterModalData);
 
   useEffect(() => {
     return () => {
       dispatch.twitterModalModel.clearTwitterModalState();
     };
   }, []);
-
+  console.log(copied);
   return (
     <ModalContainer isOpen={isOpen} setModalOpen={(isOpen: boolean) => dispatch.modalModel.setModalOpen(isOpen)}>
       <Dialog.Panel className="w-full max-w-lg transform overflow-hidden rounded-2xl bg-gray-100 p-6 text-left align-middle shadow-xl transition-all">
@@ -42,61 +27,122 @@ export const TwitterModal = () => {
           Twitter Verification
         </Dialog.Title>
         <Stack spacing="5">
-          <Stack spacing="5" divider={<StackDivider />}>
-            <FormControl id="generate-hash">
-              <Stack direction={'row'} spacing={{ base: '1.5', md: '8' }} justify={'space-between'} alignItems={'baseline'}>
-                <FormLabel variant="inline">1. Generate unique tweet</FormLabel>
-                <Flex justifyContent={'end'} alignItems={'center'} gap={3}>
-                  <Button leftIcon={<IconKey height={16} width={16} />} size={'sm'} onClick={() => dispatch.modalModel.setModalOpen(false)} bg="gray.200">
-                    Generate hash
-                  </Button>
-                </Flex>
-              </Stack>
-            </FormControl>
-            <FormControl id="copy-tweet">
-              <Stack direction={'row'} spacing={{ base: '1.5', md: '8' }} alignItems="baseline" justifyContent={'space-between'}>
-                <FormLabel variant="inline">2. Copy unique tweet</FormLabel>
-                <Flex>
-                  <Button leftIcon={<IconClipboard height={16} width={16} />} size={'sm'} onClick={() => dispatch.modalModel.setModalOpen(false)} bg="gray.200">
-                    Copy tweet
-                  </Button>
-                </Flex>
-              </Stack>
-            </FormControl>
-            <FormControl id="tweet-hash">
-              <Stack direction={'row'} spacing={{ base: '1.5', md: '8' }} alignItems="baseline" justifyContent={'space-between'}>
-                <FormLabel variant="inline">3. Tweet on your account</FormLabel>
-                <Flex>
-                  <Button leftIcon={<IconLink height={16} width={16} />} size={'sm'} onClick={() => dispatch.modalModel.setModalOpen(false)} bg="gray.200">
-                    Open Twitter
-                  </Button>
-                </Flex>
-              </Stack>
-            </FormControl>
-            <FormControl id="paste-tweet">
-              <Stack spacing={-1} direction={'row'} alignItems="baseline" justifyContent={'start'}>
-                <FormLabel variant="inline">4.</FormLabel>
-                <InputGroup size="md">
-                  <Input pr="4.5rem" type="url" placeholder="Paste tweet url here" />
-                  <InputRightElement width="4.5rem">
-                    <Button bg="green.200" h="1.75rem" size="sm" onClick={() => {}}>
-                      Verify
+          {currentStep !== TwitterStep.LINK_ACCOUNT && (
+            <Stack spacing="5" divider={<StackDivider />}>
+              <FormControl isDisabled={currentStep !== TwitterStep.GENERATE_HASH} id="generate-hash">
+                <Stack direction={'row'} spacing={{ base: '1.5', md: '8' }} justify={'space-between'} alignItems={'baseline'}>
+                  <FormLabel variant="inline">1. Generate unique tweet</FormLabel>
+                  <Flex justifyContent={'end'} alignItems={'center'} gap={3}>
+                    <Button
+                      isLoading={loading && currentStep === TwitterStep.GENERATE_HASH}
+                      loadingText="Generating"
+                      disabled={currentStep !== TwitterStep.GENERATE_HASH}
+                      leftIcon={<IconKey height={16} width={16} />}
+                      size={'sm'}
+                      onClick={() => dispatch.twitterModalModel.generateHash(profileData?.wallet)}
+                      bg="gray.200"
+                    >
+                      Generate hash
                     </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </Stack>
-            </FormControl>
+                  </Flex>
+                </Stack>
+              </FormControl>
+              <FormControl isDisabled={currentStep !== TwitterStep.COPY_TWEET} id="copy-tweet">
+                <Stack direction={'row'} spacing={{ base: '1.5', md: '8' }} alignItems="baseline" justifyContent={'space-between'}>
+                  <FormLabel variant="inline">2. Copy unique tweet</FormLabel>
+                  <Flex>
+                    <Transition
+                      show={copied}
+                      enter="transition-opacity duration-75"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="transition-opacity duration-150"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                      className="text-[0.75rem] my-auto px-2 mx-2 font-bold text-green-400 rounded-lg"
+                      as="div"
+                      children={<IconCheck height={16} width={16} />}
+                    />
+                    <Button
+                      disabled={currentStep !== TwitterStep.COPY_TWEET}
+                      leftIcon={<IconClipboard height={16} width={16} />}
+                      size={'sm'}
+                      onClick={() => {
+                        dispatch.twitterModalModel.setCopied(true);
+                        navigator.clipboard.writeText(hashedTweet);
+                        setTimeout(() => {
+                          dispatch.twitterModalModel.setCopied(false);
+                          dispatch.twitterModalModel.setCurrentStep(TwitterStep.TWEET_HASH);
+                        }, 1000);
+                      }}
+                      bg="gray.200"
+                    >
+                      Copy tweet
+                    </Button>
+                  </Flex>
+                </Stack>
+              </FormControl>
+              <FormControl isDisabled={currentStep !== TwitterStep.TWEET_HASH} id="tweet-hash">
+                <Stack direction={'row'} spacing={{ base: '1.5', md: '8' }} alignItems="baseline" justifyContent={'space-between'}>
+                  <FormLabel variant="inline">3. Tweet on your account</FormLabel>
+                  <Flex>
+                    <Button
+                      disabled={currentStep !== TwitterStep.TWEET_HASH}
+                      leftIcon={<IconLink height={16} width={16} />}
+                      size={'sm'}
+                      onClick={() => {
+                        window.open(windowParams[0], windowParams[1], windowParams[2]);
+                        setTimeout(() => {
+                          dispatch.twitterModalModel.setCurrentStep(TwitterStep.VERIFY_TWEET);
+                        }, 1000);
+                      }}
+                      bg="gray.200"
+                    >
+                      Open Twitter
+                    </Button>
+                  </Flex>
+                </Stack>
+              </FormControl>
+              <FormControl isDisabled={currentStep !== TwitterStep.VERIFY_TWEET} id="paste-tweet">
+                <Stack spacing={-1} direction={'row'} alignItems="baseline" justifyContent={'start'}>
+                  <FormLabel variant="inline">4.</FormLabel>
+                  <InputGroup size="md">
+                    <Input
+                      onChange={(e) => dispatch.twitterModalModel.setPastedTweetUrl(e.target.value)}
+                      pr="4.5rem"
+                      type="url"
+                      placeholder="Paste tweet url here"
+                    />
+                    <InputRightElement width="4.5rem">
+                      <Button
+                        isLoading={loading && currentStep === TwitterStep.VERIFY_TWEET}
+                        disabled={currentStep !== TwitterStep.VERIFY_TWEET || !pastedTweetUrl}
+                        bg="green.200"
+                        h="1.75rem"
+                        size="sm"
+                        onClick={() => dispatch.twitterModalModel.verifyTweet(pastedTweetUrl)}
+                      >
+                        Verify
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </Stack>
+              </FormControl>
+            </Stack>
+          )}
+          {currentStep === TwitterStep.LINK_ACCOUNT && (
             <FormControl id="verify-confirm">
-              <Flex gap={3} flexDirection={'column'} alignItems={'center'}>
+              <Flex gap={3} flexDirection={'column'}>
                 <FormLabel variant="inline">5. Verify and link accounts</FormLabel>
-                <Flex gap={1} justifyContent="center" alignItems={'center'}>
-                  <IconAlertCircle height={18} width={18} /> Artist Disclaimer
+                <Flex gap={2} alignItems={'center'}>
+                  <IconAlertCircle height={18} width={18} />
+                  <Text>Artist Disclaimer</Text>
                 </Flex>
-                <Text textAlign={'center'} fontSize="sm">
+                <Text fontSize="sm">
                   To maintain artist identity, if you are featured on a heds compliation, your twitter handle will be linked to your account indefinitely.
                 </Text>
               </Flex>
-              <Flex my={5} gap={4} direction={'row'} justifyContent="center" alignItems={'center'}>
+              <Flex alignItems={'center'} my={5} gap={4} direction={'row'}>
                 <Tag size="lg" colorScheme="blue" borderRadius="full">
                   <Avatar
                     src={profileData?.profilePicture}
@@ -113,12 +159,12 @@ export const TwitterModal = () => {
                 </Tag>
               </Flex>
             </FormControl>
-          </Stack>
-          <Flex justifyContent={'center'} gap={2} className="mt-6">
+          )}
+          <Flex gap={2} className="mt-6">
             <Button onClick={() => dispatch.modalModel.setModalOpen(false)} bg="gray.200">
               Back
             </Button>
-            <Button bg="green.200">Verify</Button>
+            {currentStep === TwitterStep.LINK_ACCOUNT && <Button bg="green.200">Confirm</Button>}
           </Flex>
         </Stack>
       </Dialog.Panel>
--- src/modules/profile/models/twitterModalModel.ts
+++ src/modules/profile/models/twitterModalModel.ts
@@ -1,30 +1,35 @@
 import type { RootModel } from '@/models';
 import { createModel } from '@rematch/core';
+import { sha256 } from 'js-sha256';
 const TWITTER_AUTH_CLOUD_FN = 'https://us-central1-heds-34ac0.cloudfunctions.net/twitterAuth/';
-const VERIFICATION_MESSAGE = 'verification for @hedsDAO HDS';
-// const winTank = [`https://twitter.com/intent/tweet?text=${HASHED_TWEET}`, "mywin", "width=700,height=700"];
 
-enum TwitterStep {
-  NOT_VERIFIED = 0,
-  VERIFYING,
-  LINKING,
+export enum TwitterStep {
+  GENERATE_HASH = 0,
+  COPY_TWEET,
+  TWEET_HASH,
+  VERIFY_TWEET,
+  LINK_ACCOUNT,
   COMPLETE,
   ERROR,
 }
 
 class TwitterModalState {
-  currentStep: TwitterStep = TwitterStep.NOT_VERIFIED;
+  currentStep: TwitterStep = TwitterStep.GENERATE_HASH;
   loading = false;
   tweetUrl = '';
   twitterHandle = '';
   userHash = '';
   hashedTweet = '';
   error = '';
+  copied = false;
+  pastedTweetUrl = '';
+  windowParams: [string, string, string];
 }
 
 export const twitterModalModel = createModel<RootModel>()({
   state: {
-    currentStep: TwitterStep.NOT_VERIFIED,
+    currentStep: TwitterStep.GENERATE_HASH,
+    copied: false,
   } as TwitterModalState,
   reducers: {
     setCurrentStep: (state, currentStep: TwitterStep) => ({ ...state, currentStep }),
@@ -32,7 +37,36 @@ export const twitterModalModel = createModel<RootModel>()({
     setTweetUrl: (state, tweetUrl: string) => ({ ...state, tweetUrl }),
     setUserHash: (state, userHash: string) => ({ ...state, userHash }),
     setHashedTweet: (state, hashedTweet: string) => ({ ...state, hashedTweet }),
+    setCopied: (state, copied: boolean) => ({ ...state, copied }),
+    setPastedTweetUrl: (state, pastedTweetUrl: string) => ({ ...state, pastedTweetUrl }),
+    setWindowParams: (state, windowParams: [string, string, string]) => ({ ...state, windowParams }),
     clearTwitterModalState: (state) => new TwitterModalState(),
   },
-  effects: (dispatch) => ({}),
+  effects: (dispatch) => ({
+    async generateHash(wallet: string) {
+      this.setLoading(true);
+      const VERIFICATION_MESSAGE = 'verification for @hedsDAO HDS';
+      const userHash = sha256(wallet);
+      const hashedTweet = VERIFICATION_MESSAGE + userHash;
+      this.setUserHash(userHash);
+      this.setHashedTweet(hashedTweet);
+      const windowTank = [`https://twitter.com/intent/tweet?text=${hashedTweet}`, 'mywin', 'width=700,height=700'];
+      this.setWindowParams(windowTank);
+      setTimeout(() => {
+        this.setLoading(false);
+        this.setCurrentStep(TwitterStep.COPY_TWEET);
+      }, 1000);
+    },
+    async verifyTweet(pastedTweetUrl: string) {
+      this.setLoading(true);
+      const urlTank = pastedTweetUrl.split("/");
+			let tweetId = urlTank[urlTank.length - 1];
+			tweetId = tweetId.slice(0, 19);
+      console.log(tweetId)
+      setTimeout(() => {
+        this.setLoading(false);
+        this.setCurrentStep(TwitterStep.LINK_ACCOUNT);
+      }, 2000)
+    }
+  }),
 });
