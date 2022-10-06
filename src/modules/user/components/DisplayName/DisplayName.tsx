0fda6a03926e2d97ef4b69de59194c34dc5a7e16
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2022-10-06 17:12:09 -0500

    styling

--- src/modules/user/components/DisplayName/DisplayName.tsx
+++ src/modules/user/components/DisplayName/DisplayName.tsx
@@ -5,7 +5,7 @@ const DisplayName = ({ loading, userData }: { loading: boolean; userData: User }
   return (
     <Box>
       <Skeleton rounded="md" height="1.5rem" fadeDuration={2} isLoaded={!loading}>
-        <Heading size={'lg'} data-testid="user-display-name">
+        <Heading my={2} py={2} size={'lg'} data-testid="user-display-name">
           {userData?.displayName}
         </Heading>
       </Skeleton>
