62c801df32deef2ac3bdb9d30c4181436cffbe26
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2022-10-06 17:31:00 -0500

    more styling

--- src/modules/user/components/UserDescription/UserDescription.tsx
+++ src/modules/user/components/UserDescription/UserDescription.tsx
@@ -5,7 +5,7 @@ import { Box, Skeleton, Text } from '@chakra-ui/react';
 const UserDescription = ({ loading, userData }: { loading: boolean; userData: User }) => {
   return (
     <Fragment>
-      <Box py={2}>
+      <Box>
         <Skeleton rounded="md" height="10px" fadeDuration={2} isLoaded={!loading}>
           <Text data-testid="user-description" fontSize={'sm'}>
             {userData?.description || '...'}
