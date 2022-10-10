58c787b9d035be490ba902048f3a096197e33ab7
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2022-10-10 17:36:04 -0500

    Update Collection.tsx

--- src/common/user/Collection/Collection.tsx
+++ src/common/user/Collection/Collection.tsx
@@ -10,20 +10,20 @@ const Collection = ({ userData, loading }: { userData: User; loading: boolean })
         <Heading fontSize={'3xl'}>Collection</Heading>
         <RefreshCollectionButton userData={userData} loading={loading} />
       </Flex>
-      <Grid className="px-3 py-3" templateColumns={{base: "repeat(3, 1fr)", md: "repeat(5, 1fr)"}} gap={4}>
+      <Grid className="px-3 py-3" templateColumns={{ base: 'repeat(3, 1fr)', md: 'repeat(5, 1fr)' }} gap={4}>
         {!isEmpty(userData?.collection) &&
           Object.values(userData?.collection).map((collectionItem) => {
             return (
               <GridItem key={collectionItem.name + collectionItem.quantity}>
                 <Skeleton w="full" h="full" isLoaded={!loading} fadeDuration={2}>
-                  <img
-                    src={collectionItem.image}
-                    className="object-cover lg:min-h-[12rem] lg:max-h-[12rem] lg:min-w-[12rem] lg:max-w-[12rem] aspect-square rounded-lg object-center hover:opacity-75"
-                  />
-                  <Flex justifyContent={'space-between'}>
+                  <div className="relative">
+                    <img
+                      src={collectionItem.image}
+                      className="object-cover lg:min-h-[12rem] lg:max-h-[12rem] lg:min-w-[12rem] lg:max-w-[12rem] aspect-square rounded-lg object-center hover:opacity-75"
+                    />
+                    <div className="absolute top-2 right-0 py-1 px-2 bg-white bg-opacity-70">x{collectionItem.quantity}</div>
                     <span>{collectionItem.name}</span>
-                    <span>x{collectionItem.quantity}</span>
-                  </Flex>
+                  </div>
                 </Skeleton>
               </GridItem>
             );
