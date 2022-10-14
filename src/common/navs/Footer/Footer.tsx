4ae404663005abd4b6b63535b0cd484bef3fb2ac
Ramtin Khoee <rahmteen@heds.io>
2022-10-14 13:12:30 -0700

    banner update pt 2"

--- src/common/navs/Footer/Footer.tsx
+++ src/common/navs/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="p-3 bg-white rounded-lg md:flex items-center md:justify-between md:py-5 dark:bg-gray-800 mt-5 md:max-w-7xl mx-auto">
+    <footer className="py-3 bg-[#f5f5f5] rounded-lg md:flex items-center md:justify-between md:py-5 dark:bg-gray-800">
+      <div className="mx-auto max-w-7xl flex md:justify-between w-full">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 font-serif px-2">
         2022{' '}
         <a href="https://heds.app/" className="hover:underline">
@@ -20,6 +21,7 @@ const Footer = () => {
           </a>
         </li>
       </ul>
+      </div>
     </footer>
   );
 };
