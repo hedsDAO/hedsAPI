e4fe15f6ec648499934612376ff5b78912a126bc
Ramtin Khoee <rahmteen@heds.io>
2022-11-01 21:53:59 -0700

    timeline nits

--- src/pages/listen/components/Timeline/Timeline.tsx
+++ src/pages/listen/components/Timeline/Timeline.tsx
@@ -1,21 +1,21 @@
-import { Dispatch, RootState } from '@/store';
+import { RootState } from '@/store';
 import { classNames } from '@/utils';
-import { Box, Container, Flex } from '@chakra-ui/react';
+import { Box, Container, Flex, ListItem, Text, UnorderedList } from '@chakra-ui/react';
 import { CheckIcon } from '@heroicons/react/24/solid';
-import { useDispatch, useSelector } from 'react-redux';
+import { IconHourglass } from '@tabler/icons';
+import { useSelector } from 'react-redux';
 
 const Timeline = () => {
-  const dispatch = useDispatch<Dispatch>();
   const { timeline } = useSelector((state: RootState) => state.hedstapeModel);
   return (
     <>
       <div className="lg:border-t lg:border-b lg:border-gray-200">
         <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8" aria-label="Progress">
-          <ol role="list" className="overflow-hidden rounded-md lg:flex lg:rounded-none lg:border-l lg:border-r lg:border-gray-200">
+          <UnorderedList role="list" className="overflow-hidden rounded-md lg:flex lg:rounded-none lg:border-l lg:border-r lg:border-gray-200">
             {timeline &&
               Object.entries(timeline).map(([step, data], i) => (
-                <li key={data.name} className="relative overflow-hidden lg:flex-1">
-                  <div
+                <ListItem key={data.name} flex={1} overflow={{ lg: 'hidden' }} pos={'relative'}>
+                  <Container
                     className={classNames(
                       i === 0 ? 'border-b-0 rounded-t-md' : '',
                       i === Object.keys(timeline).length - 1 ? 'border-t-0 rounded-b-md' : '',
@@ -23,72 +23,59 @@ const Timeline = () => {
                     )}
                   >
                     {data.status === 'closed' ? (
-                      <Container className="group">
-                        <span
+                      <Container className="group transition-all ease-in-out">
+                        <Box
                           className="absolute top-0 left-0 h-full w-1 bg-transparent group-hover:bg-gray-200 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full"
                           aria-hidden="true"
                         />
-                        <Flex pl={i == 0 && { lg: '9' }} px={6} py={5} fontSize={'sm'}>
+                        <Flex justifyContent={'center'} alignItems={'center'} pl={i == 0 && { lg: '9' }} px={4} py={5} gap={2} fontSize={'sm'}>
                           <Box flexShrink={0}>
-                            <span className="flex h-8 w-8 items-center justify-center rounded-full bg-indigo-600">
-                              <CheckIcon className="h-4 w-4 text-white" aria-hidden="true" />
-                            </span>
+                            <Box className="flex h-7 w-7 items-center justify-center rounded-full bg-green-200">
+                              <CheckIcon className="h-4 w-4 text-gray-600" aria-hidden="true" />
+                            </Box>
                           </Box>
+                          <Flex flexDirection={'column'} className="mt-0.5 ml-4 flex min-w-0 flex-col" gap={1}>
+                            <Text className="text-sm font-medium">{data.name}</Text>
+                            <Text className="text-xs font-medium text-gray-500">{data.description}</Text>
+                          </Flex>
                         </Flex>
                       </Container>
-                    ) : // <div className="group">
-                    //   <span
-                    //     className="absolute top-0 left-0 h-full w-1 bg-transparent group-hover:bg-gray-200 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full"
-                    //     aria-hidden="true"
-                    //   />
-                    //   <span className={classNames(i !== 0 ? 'lg:pl-9' : '', 'px-6 py-5 flex items-start text-sm font-medium')}>
-                    //     <span className="flex-shrink-0">
-                    //       <span className="flex h-8 w-8 items-center justify-center rounded-full bg-indigo-600">
-                    //         <CheckIcon className="h-4 w-4 text-white" aria-hidden="true" />
-                    //       </span>
-                    //     </span>
-                    //     <span className="mt-0.5 ml-4 flex min-w-0 flex-col">
-                    //       <span className="text-sm font-medium">{data.name}</span>
-                    //       <span className="text-xs font-medium text-gray-500">{data.description}</span>
-                    //       <span>
-                    //         <i className="fa-solid fa-ellipsis" />
-                    //       </span>
-                    //     </span>
-                    //   </span>
-                    // </div>
-                    data.status === 'current' ? (
-                      <div>
-                        <span className="absolute top-0 left-0 h-full w-1 bg-indigo-600 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full" aria-hidden="true" />
-                        <span className={classNames(i !== 0 ? 'lg:pl-9' : '', 'px-6 py-5 flex items-start text-sm font-medium')}>
-                          <span className="flex-shrink-0">
-                            <span className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-indigo-600">
-                              {/* <span className="text-indigo-600">{step.id}</span> */}
-                            </span>
-                          </span>
-                          <span className="mt-0.5 ml-4 flex min-w-0 flex-col">
-                            <span className="text-sm font-medium text-indigo-600">{data.name}</span>
-                            <span className="text-xs font-medium text-gray-500">{data.description}</span>
-                          </span>
-                        </span>
-                      </div>
+                    ) : data.status === 'closed' ? (
+                      <Container className="group transition-all ease-in-out">
+                        <Box
+                          className="absolute top-0 left-0 h-full w-1 group-hover:bg-green-400 bg-green-600 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full"
+                          aria-hidden="true"
+                        />
+                        <Flex justifyContent={'center'} alignItems={'center'} pl={i == 0 && { lg: '9' }} px={4} py={5} gap={2} fontSize={'sm'}>
+                          <Box flexShrink={0}>
+                            <Box className="flex h-7 w-7 items-center justify-center rounded-full border-2 border-green-600">
+                              <CheckIcon className="h-4 w-4 text-gray-600" aria-hidden="true" />
+                            </Box>
+                          </Box>
+                          <Flex flexDirection={'column'} className="mt-0.5 ml-4 flex min-w-0 flex-col" gap={1}>
+                            <Text className="text-sm font-medium">{data.name}</Text>
+                            <Text className="text-xs font-medium text-gray-500">{data.description}</Text>
+                          </Flex>
+                        </Flex>
+                      </Container>
                     ) : (
-                      <div className="group">
-                        <span
+                      <Container className="group transition-all ease-in-out">
+                        <Box
                           className="absolute top-0 left-0 h-full w-1 bg-transparent group-hover:bg-gray-200 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full"
                           aria-hidden="true"
                         />
-                        <span className={classNames(i !== 0 ? 'lg:pl-9' : '', 'px-6 py-5 flex items-start text-sm font-medium')}>
-                          <span className="flex-shrink-0">
-                            <span className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-gray-300">
-                              {/* <span className="text-gray-500">{step.id}</span> */}
-                            </span>
-                          </span>
-                          <span className="mt-0.5 ml-4 flex min-w-0 flex-col">
-                            <span className="text-sm font-medium text-gray-500">{data.name}</span>
-                            <span className="text-xs font-medium text-gray-500">{data.description}</span>
-                          </span>
-                        </span>
-                      </div>
+                        <Flex justifyContent={'center'} alignItems={'center'} pl={i == 0 && { lg: '9' }} px={4} py={5} gap={2} fontSize={'sm'}>
+                          <Box flexShrink={0}>
+                            <Box className="flex h-7 w-7 items-center justify-center rounded-full border-2 border-gray-300">
+                              <IconHourglass className="h-4 w-4 text-gray-600" aria-hidden="true" />
+                            </Box>
+                          </Box>
+                          <Flex flexDirection={'column'} className="mt-0.5 ml-4 flex min-w-0 flex-col" gap={1}>
+                            <Text className="text-sm font-medium">{data.name}</Text>
+                            <Text className="text-xs font-medium text-gray-500">{data.description}</Text>
+                          </Flex>
+                        </Flex>
+                      </Container>
                     )}
                     {i !== 0 ? (
                       <>
@@ -99,10 +86,10 @@ const Timeline = () => {
                         </div>
                       </>
                     ) : null}
-                  </div>
-                </li>
+                  </Container>
+                </ListItem>
               ))}
-          </ol>
+          </UnorderedList>
         </nav>
       </div>
     </>
