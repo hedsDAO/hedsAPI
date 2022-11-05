96c24599600a9b8c930cb09e1f04da1ccce4bd52
Ramtin Khoee <rahmteen@heds.io>
2022-11-05 10:14:25 -0700

    updated profile with css nits

--- src/pages/listen/components/Tracks/Tracks.tsx
+++ src/pages/listen/components/Tracks/Tracks.tsx
@@ -3,7 +3,8 @@ import { TrackMetadata, User, UserRoles } from '@/models/common';
 import { formatTime } from '@/utils';
 import { Dispatch, RootState } from '@/store';
 import { Avatar, Container, Flex, Heading, IconButton, Stack } from '@chakra-ui/react';
-import { useDispatch, useSelector } from 'react-redux';import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
 
 const Tracks = () => {
   const dispatch = useDispatch<Dispatch>();
@@ -19,7 +20,7 @@ const Tracks = () => {
   };
 
   return (
-    <Container px={{ base: 4, md: 0 }} maxW="7xl">
+    <Container px={{ base: 2, md: 0 }} maxW="7xl">
       <Heading fontSize={{ base: '2xl', md: '3xl' }} mx="auto" maxWidth={'7xl'} textAlign={'start'}>
         Tracks
       </Heading>
@@ -27,22 +28,44 @@ const Tracks = () => {
         <Stack spacing="2">
           {tracks?.map((track) => {
             return (
-              <li className={`text-sm text-gray-600 rounded-md border-green-200/60 border`} key={id + track?.displayName}>
+              <li className={`text-sm text-gray-600 rounded-md border-neutral-200 border`} key={id + track?.displayName}>
                 <div className="flex justify-between items-center gap-x-2 w-full hover:bg-gray-50 px-2 py-2 rounded-md">
                   <Flex alignItems={'center'} gap={2}>
+                    <IconButton
+                      bg="blue.200"
+                      borderColor={'blue.300'}
+                      textColor={'blackAlpha.800'}
+                      size={{ base: 'xs', lg: 'xs' }}
+                      aria-label="play"
+                      icon={<i className="fa-solid fa-play"></i>}
+                      onClick={() => handlePlay(track?.tracks?.[space][tape][id])}
+                      className="flex-shrink-0 mr-1 border"
+                    />
                     <Avatar to={`/u/${track.wallet}`} as={Link} borderRadius="full" boxSize="25px" src={track?.profilePicture} />
                     <div className="min-w-0 flex-1 sm:flex text-xs text-neutral-800 -mr-1">{track?.tracks?.[space][tape][id]?.track}.</div>
                     <span className="text-xs font-medium">{track?.displayName}</span>
                   </Flex>
                   <Flex alignItems={'center'} gap={2}>
-                    <span className="mr-2 sm:mr-4 whitespace-nowrap">{formatTime(track?.tracks?.[space][tape][id]?.duration)}</span>
-                    <IconButton 
-                      bg="blue.200" size="xs" aria-label="play" icon={<i className="fa-solid fa-play"></i>}
-                      onClick={() => handlePlay(track?.tracks?.[space][tape][id])} className="flex-shrink-0"
+                    <span className="mr-2 sm:mr-4 whitespace-nowrap text-xs lg:text-sm">{formatTime(track?.tracks?.[space][tape][id]?.duration)}</span>
+                    <IconButton
+                      bg="red.200"
+                      borderColor={'red.300'}
+                      textColor={'blackAlpha.800'}
+                      size={{ base: 'xs', lg: 'xs' }}
+                      aria-label="like"
+                      icon={<i className="fa-solid fa-heart"></i>}
+                      onClick={() => handlePlay(track?.tracks?.[space][tape][id])}
+                      className="flex-shrink-0 border"
                     />
-                    <IconButton 
-                      bg="green.200" size="xs" aria-label="add queue" icon={<i className="fa-solid fa-layer-plus"></i>}
-                      onClick={() => addToQueue(track?.tracks?.[space][tape][id])} className="flex-shrink-0" 
+                    <IconButton
+                      bg="green.200"
+                      borderColor={'green.300'}
+                      textColor={'blackAlpha.800'}
+                      size={{ base: 'xs', lg: 'xs' }}
+                      aria-label="add queue"
+                      icon={<i className="fa-solid fa-layer-plus"></i>}
+                      onClick={() => addToQueue(track?.tracks?.[space][tape][id])}
+                      className="flex-shrink-0 border"
                     />
                   </Flex>
                 </div>
