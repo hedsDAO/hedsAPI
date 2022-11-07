5479e46bba601f47e2b7a9c8613ffe678f82981b
cgreer011 <camgreer01@gmail.com>
2022-11-07 14:32:58 -0500

    add rematch persist for audio model

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,22 @@
 import { init, RematchDispatch, RematchRootState } from '@rematch/core';
 import loadingPlugin, { ExtraModelsFromLoading } from '@rematch/loading';
+import persistPlugin from "@rematch/persist";
+import storage from "redux-persist/lib/storage";
 import selectPlugin from '@rematch/select';
 import { models, RootModel } from './models';
 type FullModel = ExtraModelsFromLoading<RootModel>;
 
+/** Local Storage Persist for Audio Model */
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 2,
+  whitelist: ["audioModel"]
+};
+
 export const store = init<RootModel, FullModel>({
   models,
-  plugins: [selectPlugin(), loadingPlugin()],
+  plugins: [selectPlugin(), loadingPlugin(), persistPlugin(persistConfig)],
   redux: {
     devtoolOptions: {
       disabled: process.env.NODE_ENV === 'production',
