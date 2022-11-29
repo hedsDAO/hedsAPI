d2857686741bc7e86e4812d641f40074019717e3
Ramtin Khoee <rahmteen@heds.io>
2022-11-28 16:58:04 -0800

    user page nits

--- src/common/user/RefreshCollectionButton/RefreshCollectionButton.tsx
+++ src/common/user/RefreshCollectionButton/RefreshCollectionButton.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, RootState, store } from '@/store';
+import { Dispatch, store } from '@/store';
 import { formatReadContractArgs, isEmpty } from '@/utils';
 import { Button } from '@chakra-ui/react';
 import { IconRefresh } from '@tabler/icons';
@@ -6,13 +6,12 @@ import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useContractReads } from 'wagmi';
 import { Result } from 'ethers/lib/utils';
-import { useLocation } from 'react-router-dom';
 
 const RefreshCollectionButton = () => {
   const dispatch = useDispatch<Dispatch>();
   const hedsTapes = useSelector(store.select.tapesModel.selectAllHedsTapes);
   const wallet = useSelector(store.select.userModel.selectCurrentUserWallet);
-  const { pathname } = useLocation();
+  const connectedWallet = useSelector(store.select.userModel.selectConnectedUserWallet);
   const { data, refetch } = useContractReads({
     contracts: formatReadContractArgs(wallet, hedsTapes),
     allowFailure: true,
@@ -30,8 +29,8 @@ const RefreshCollectionButton = () => {
   const handleUpdateCollection = useCallback(
     (data: Result[]) => {
       if (data?.length && !isEmpty(hedsTapes)) {
-        if (pathname.includes('/u')) dispatch.userModel.updateCurrentUserCollection([wallet?.toLowerCase(), data, hedsTapes]);
-        else if (pathname.includes('/profile')) dispatch.userModel.updateConnectedUserCollection([wallet?.toLowerCase(), data, hedsTapes]);
+        if (wallet === connectedWallet) dispatch.userModel.updateConnectedUserCollection([wallet?.toLowerCase(), data, hedsTapes]);
+        dispatch.userModel.updateCurrentUserCollection([wallet?.toLowerCase(), data, hedsTapes]);
       }
     },
     [data],
