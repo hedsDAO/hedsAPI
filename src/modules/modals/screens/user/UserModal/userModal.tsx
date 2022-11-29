8581384d216475b3ac7aff71efabe8695bd3c9ac
Ramtin Khoee <rahmteen@heds.io>
2022-11-29 09:42:01 -0800

    updated light/dark mode and connected user modal

--- src/modules/modals/screens/user/UserModal/userModal.tsx
+++ src/modules/modals/screens/user/UserModal/userModal.tsx
@@ -1,10 +1,10 @@
 import { ModalContainer, ModalHeader } from '@/modules/modals/components';
 import { Dispatch, RootState, store } from '@/store';
-import { Container, Flex, IconButton, useColorMode } from '@chakra-ui/react';
+import { Avatar, Button, color, Container, Divider, Flex, Heading, HStack, IconButton, Text, useColorMode } from '@chakra-ui/react';
 import { useDispatch, useSelector } from 'react-redux';
 import { IconHomeCog, IconPower } from '@tabler/icons';
 import { formatWallet } from '@/utils';
-import { useDisconnect } from 'wagmi';
+import { useAccount, useDisconnect, useEnsName } from 'wagmi';
 import { Switch } from '@headlessui/react';
 
 const UserModal = () => {
@@ -13,31 +13,42 @@ const UserModal = () => {
   const { isOpen } = useSelector((state: RootState) => state.modalModel);
   const wallet = useSelector(store.select.userModel.selectConnectedUserWallet);
   const { disconnect } = useDisconnect();
+  const { address } = useAccount();
+  const { data: ensName } = useEnsName({ address: address });
+
   return (
     <ModalContainer size={'sm'} isOpen={isOpen} setModalOpen={(isOpen: boolean) => dispatch.modalModel.setModalOpen(isOpen)}>
-      <ModalHeader Icon={IconHomeCog} title={formatWallet(wallet)} />
+      <ModalHeader Icon={IconHomeCog} title={'Account'} />
       <Flex direction={'column'} alignItems={'center'} justifyContent="center">
-        <IconButton
+        <Heading mb={2} fontSize={'md'}>
+          Connected:
+        </Heading>
+        <Text mb={4}>{ensName || formatWallet(address)}</Text>
+        <Button
+          size="sm"
+          variant={'outline'}
           onClick={() => {
             disconnect();
             dispatch.userModel.clearConnectedUserState();
             dispatch.modalModel.setModalOpen(false);
           }}
-          as={IconPower}
+          leftIcon={<IconPower height="18" width="18" />}
           aria-label="disconnect"
-        />
+        >
+          disconnect
+        </Button>
+        <Divider my={5} />
+        <Heading mb={2} fontSize={'md'}>
+          Experimental:
+        </Heading>
+        <Text mb={4}>{'dark mode'}</Text>
         <Switch
           checked={colorMode === 'dark'}
           onChange={toggleColorMode}
-          className={`bg-neutral-300 relative inline-flex h-6 w-11 lg:h-7 lg:w-12 items-center rounded-full`}
+          className={`${colorMode === 'dark' ? 'bg-blue-600' : 'bg-gray-200'} relative inline-flex h-6 w-11 items-center rounded-full`}
         >
-          <span
-            className={`${
-              colorMode === 'dark' ? 'translate-x-6 bg-neutral-900' : 'translate-x-1 bg-neutral-900'
-            } flex items-center text-center justify-center h-4 w-4 lg:h-5 lg:w-5 transform rounded-full transition`}
-          >
-            {colorMode === 'light' ? 'Dark' : 'Light'}
-          </span>
+          <span className="sr-only">Enable notifications</span>
+          <span className={`${colorMode === 'dark' ? 'translate-x-6' : 'translate-x-1'} inline-block h-4 w-4 transform rounded-full bg-white transition`} />
         </Switch>
       </Flex>
     </ModalContainer>
