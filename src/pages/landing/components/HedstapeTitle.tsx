b8f49a54b370a49ac461a771ded9f3a2e62d5ebb
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2022-11-30 16:55:11 -0600

    code clean up

---
+++ src/pages/landing/components/HedstapeTitle.tsx
@@ -0,0 +1,10 @@
+export const HedsTapeTitle = () => (
+  <>
+    <div className="flex flex-row mt-20em">
+      <p>THE</p>
+      <p className="text-9xl font-bold">hedsTAPE</p>
+      <p className="mt-auto">SERIES</p>
+    </div>
+    <p>curated samples from world renowned creatives</p>
+  </>
+);
