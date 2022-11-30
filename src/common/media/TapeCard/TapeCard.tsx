396a8210ac488eff3b3173ff97dd53361fcaa556
Ramtin Khoee <rahmteen@heds.io>
2022-11-29 16:43:06 -0800

    testing dark mode

--- src/common/media/TapeCard/TapeCard.tsx
+++ src/common/media/TapeCard/TapeCard.tsx
@@ -1,12 +1,14 @@
 import { UserCollectionItem } from '@/models/common';
-import { Divider, Skeleton, Stack } from '@chakra-ui/react';
+import { Divider, Skeleton, Stack, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 const TapeCard = ({ item, loading }: { item: UserCollectionItem; loading: boolean }) => {
+  const { toggleColorMode } = useColorMode();
+  const color = useColorModeValue('white', 'gray.800');
   const generateUrl = (str: string) => 'http://localhost:8090/listen/heds/' + str.split(' ')[0].toLowerCase() + `/${Math.floor(+str.split(' ')[1])}`;
   return (
-    <Stack divider={<Divider />} border={'1px'} borderColor={'gray.200'} rounded="lg" p={2} shadow="md">
+    <Stack divider={<Divider />} border={'1px'} borderColor={color} rounded="lg" p={2} shadow="md">
       <Skeleton w="full" h="fit-content" rounded="lg" isLoaded={!loading} fadeDuration={1}>
         <div className="relative">
           <a href={generateUrl(item.name)}>
