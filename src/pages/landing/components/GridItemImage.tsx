76b9d4f49d12e77bfd5656cc2f5ddfec2d7dffce
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2022-12-19 15:11:47 -0600

    more stats

---
+++ src/pages/landing/components/GridItemImage.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import { Center, GridItem, Icon, VStack } from '@chakra-ui/react';
+import { TablerIcon } from '@tabler/icons';
+import { Number } from '@/pages/landing/components/Number';
+
+interface OwnProps {
+  num: number;
+  text: string;
+  icon: TablerIcon;
+}
+
+export const GridItemImage: FC<OwnProps> = ({ num, text, icon }) => {
+  return (
+    <Center borderColor="black" border="1px" borderRadius="0.3rem" borderStyle="dotted">
+      <GridItem>
+        <VStack>
+          <>
+            <Number inputNum={num} color="black" size="3rem" />
+            {text}
+            <Icon as={icon} boxSize={20} />
+          </>
+        </VStack>
+      </GridItem>
+    </Center>
+  );
+};
