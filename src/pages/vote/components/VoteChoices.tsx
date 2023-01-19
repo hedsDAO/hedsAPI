8a99b637f057cd3912c85bd8a2863df2e98bfd68
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-01-19 15:00:16 -0500

    Users can give a score to each choice

--- src/pages/vote/components/VoteChoices.tsx
+++ src/pages/vote/components/VoteChoices.tsx
@@ -1,12 +1,18 @@
 import { Dispatch, store } from '@/store';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { VoteChoiceCard } from '@/common/media';
-import { Choice, ProposalState } from 'hedsvote';
-import { Container, Flex, Grid, Heading, Stack } from '@chakra-ui/react';
 
+// Components
+import { VoteChoiceCard } from '@/common/media';
+import { Container, Flex, Grid, Heading, Stack } from '@chakra-ui/react';
 import { SubmissionCard } from './SubmissionCard';
 import { SelectedSubmission } from './SelectedSubmission';
+
+// Models
+import { Choice, ProposalState } from 'hedsvote';
+import { SubmissionChoice } from '../store/voteModel';
+
+// Styles
 import styled from 'styled-components';
 
 export const VoteChoices = () => {
@@ -14,7 +20,25 @@ export const VoteChoices = () => {
   const choices = useSelector(store.select.voteModel.selectProposalChoices);
   const proposal = useSelector(store.select.voteModel.selectProposal);
   const proposalState = proposal?.state;
-  const [voteOptions, setVoteOptions] = useState<Choice[]>([]);
+  const [voteOptions, setVoteOptions] = useState<SubmissionChoice[]>([]);
+
+  const handleSelectedSubmission = (choice: Choice) => {
+    if (!voteOptions.find((c) => c.id === choice.id)) {
+      const choiceWithScore = { ...choice, score: 0 };
+      setVoteOptions([...voteOptions, choiceWithScore]);
+    }
+  };
+
+  const handleScoreChange = (choice: SubmissionChoice, type: string) => {
+    const newVoteOptions = voteOptions.map((c) => {
+      if (c.id === choice.id) {
+        return { ...c, score: type === 'add' ? c.score + 1 : c.score > 0 ? c.score - 1 : 0 };
+      } else {
+        return c;
+      }
+    });
+    setVoteOptions(newVoteOptions);
+  };
 
   // need to add heading to CLOSED option
   return (
@@ -35,15 +59,7 @@ export const VoteChoices = () => {
             <Grid pt={6} templateColumns={{ base: 'repeat(3, 1fr)', lg: 'repeat(5, 1fr)' }} gap={3}>
               {choices &&
                 choices?.map((choice: Choice) => {
-                  return (
-                    <SubmissionCard
-                      onClick={() => {
-                        if (!voteOptions.includes(choice)) return setVoteOptions([...voteOptions, choice]);
-                      }}
-                      key={choice.name + choice.image}
-                      choice={choice}
-                    />
-                  );
+                  return <SubmissionCard onClick={() => handleSelectedSubmission(choice)} key={choice.name + choice.image} choice={choice} />;
                 })}
             </Grid>
           </Container>
@@ -53,12 +69,13 @@ export const VoteChoices = () => {
               SELECTED SUBMISSIONS
             </StyledHeading>
             <Stack pt={3} my={3}>
-              {voteOptions.map((choice: Choice) => {
+              {voteOptions.map((choice: SubmissionChoice) => {
                 return (
                   <SelectedSubmission
                     handleRemoveSubmission={() => setVoteOptions(voteOptions.filter((c) => c.id !== choice.id))}
                     key={choice.name + choice.image}
                     choice={choice}
+                    handleScoreChange={handleScoreChange}
                   />
                 );
               })}
