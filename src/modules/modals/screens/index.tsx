56b7323283b93fe88cbed7d547514d1e846f26e1
cgreer011 <camgreer01@gmail.com>
2023-01-19 13:28:49 -0500

    fix suffix

--- src/modules/modals/screens/index.tsx
+++ src/modules/modals/screens/index.tsx
@@ -1,5 +1,5 @@
 export { default as ConnectModal } from './connect/ConnectModal/ConnectModal';
-export { default as UserModal } from './user/UserModal/UserModal';
+export { default as UserModal } from './user/UserModal/userModal';
 export { default as SettingsModal } from './settings/SettingsModal/SettingsModal';
 export { default as SubmitModal } from './submit/SubmitModal/SubmitModal';
 export { default as MintModal } from './mint/MintModal/MintModal';
