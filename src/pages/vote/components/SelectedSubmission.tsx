8bb9be25670d6b48181f400f502adb11f2aa379c
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-01-19 15:49:06 -0500

    Added styling

--- src/pages/vote/components/SelectedSubmission.tsx
+++ src/pages/vote/components/SelectedSubmission.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
 // Components
-import { Avatar, Flex, HStack, Stack, Text, IconButton } from '@chakra-ui/react';
+import { Avatar, Box, Flex, Grid, GridItem, HStack, Stack, Text, IconButton } from '@chakra-ui/react';
 import { IconX, IconPlus, IconMinus } from '@tabler/icons';
 
 // Models
@@ -14,21 +13,27 @@ interface OwnProps {
 
 export const SelectedSubmission = ({ choice, handleRemoveSubmission, handleScoreChange }: OwnProps) => {
   return (
-    <Flex p={2} border="1px" rounded="sm" shadow="sm" borderColor="gray.500" w="full" minW="full" gap={2} key={choice.name}>
-      <HStack spacing="4" direction="row" justify="space-between" w="full">
-        <Stack spacing="1">
-          <Avatar size="xs" src={choice.image} />
-          <Text textColor={'gray.800'} fontSize={'xs'}>
-            {choice.name}
-          </Text>
-        </Stack>
-        <HStack>
-          <IconButton aria-label="Subtract" icon={<IconMinus />} onClick={() => handleScoreChange(choice, 'subtract')} />
-          <Text>{choice.score}</Text>
-          <IconButton aria-label="Add" icon={<IconPlus />} onClick={() => handleScoreChange(choice, 'add')} />
-        </HStack>
-        <IconButton aria-label="Remove Submission" size="xs" colorScheme="red" icon={<IconX />} onClick={() => handleRemoveSubmission(choice)} />
-      </HStack>
-    </Flex>
+    <Box p={2} border="1px" rounded="sm" shadow="sm" borderColor="gray.500" w="full" minW="full" key={choice.name}>
+      <Grid templateColumns="repeat(3, 1fr)" gap="5">
+        <GridItem>
+          <Stack spacing="1">
+            <Avatar size="md" src={choice.image} />
+            <Text textColor={'gray.800'} fontSize="sm">
+              {choice.name}
+            </Text>
+          </Stack>
+        </GridItem>
+        <GridItem alignSelf="center">
+          <HStack justifyContent="center">
+            <IconButton aria-label="Subtract" icon={<IconMinus />} onClick={() => handleScoreChange(choice, 'subtract')} />
+            <Text>{choice.score}</Text>
+            <IconButton aria-label="Add" icon={<IconPlus />} onClick={() => handleScoreChange(choice, 'add')} />
+          </HStack>
+        </GridItem>
+        <GridItem justifySelf="flex-end">
+          <IconButton aria-label="Remove Submission" size="xs" colorScheme="red" icon={<IconX />} onClick={() => handleRemoveSubmission(choice)} />
+        </GridItem>
+      </Grid>
+    </Box>
   );
 };
