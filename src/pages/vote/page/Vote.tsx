8eb6abcf03b5faf00523124efe0bd73f9f82edd9
Ramtin Khoee <rahmteen@heds.io>
2023-01-28 15:02:05 -0800

    updated vote landing mock and left previous landing + comps for future ref/common

--- src/pages/vote/page/Vote.tsx
+++ src/pages/vote/page/Vote.tsx
@@ -1,9 +1,9 @@
 import { Fragment } from 'react';
 import { useParams } from 'react-router-dom';
-import VoteLandingMock from './VoteLandingMock';
+import { VoteLanding } from './OldVoteLanding';
 import { TapeDetails } from './TapeDetails';
 
 export const Vote = () => {
   const { space, tape, id } = useParams();
-  return <Fragment>{space && tape && id ? <TapeDetails /> : <VoteLandingMock />}</Fragment>;
+  return <Fragment>{space && tape && id ? <TapeDetails /> : <VoteLanding />}</Fragment>;
 };
