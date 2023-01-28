8aadccd7ecad3c7f9a78315ca099d959e908885f
Ramtin Khoee <rahmteen@heds.io>
2023-01-28 15:02:05 -0800

    updated vote landing mock and left previous landing + comps for future ref/common

--- src/pages/vote/page/VoteLanding.tsx
+++ src/pages/vote/page/VoteLanding.tsx
@@ -1,77 +1,61 @@
-import { store } from '@/store';
-import { useSelector } from 'react-redux';
-import { Box, Container, Divider, Grid, HStack, Stack, Text } from '@chakra-ui/react';
+import { Dispatch, store } from '@/store';
 import { isEmpty } from '@/utils';
-import { VoteCard } from '@/common/media';
-import { ImageWithOverlay } from '../components/ImageWithOverlay';
+import { Box, Flex, Image, Spinner } from '@chakra-ui/react';
+import { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { LandingAllVotes } from '../components/LandingAllVotes';
+import { LandingTapeCovers } from '../components/LandingTapeCovers';
+import Slider from 'react-slick';
 
-export const VoteLanding = () => {
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import hedImage from '../../../public/heddot.png';
+
+const VoteLandingMock = () => {
+  const dispatch = useDispatch<Dispatch>();
   const allHedsTapes = useSelector(store.select.tapesModel.selectAllHedsTapes);
-  const latestHedsTape = useSelector(store.select.tapesModel.selectLatestHedsTape);
+  const allProposals = useSelector(store.select.voteModel.selectAllProposals);
+  var settings = { dots: true, autoplay: true, infinite: true, speed: 500, slidesToShow: 1, slidesToScroll: 1 };
+  
+  useEffect(() => {
+    dispatch.voteModel.getAllProposals();
+  }, []);
 
   return (
-    <Container minW="full">
-      <Box>
-        <Container py={{ base: '2', md: '8' }}>
-          <HStack>
-            <Divider borderColor="gray.700" w="full" />
-            <Text fontSize="lg" fontWeight="semibold" whiteSpace="nowrap" letterSpacing={'widest'}>
-              LATEST VOTES
-            </Text>
-            <Divider borderColor="gray.700" w="full" />
-          </HStack>
-        </Container>
+    <Fragment>
+      <Box minH="75vh" minW="100vw">
+        <Box maxW="7xl" mx="auto" pb={10}>
+          {!isEmpty(allHedsTapes) && allProposals?.length ? (
+            <Slider arrows={false} {...settings}>
+              {Object.values(allHedsTapes)
+                .slice(Object.values(allHedsTapes).length - 3, Object.values(allHedsTapes).length)
+                .reverse()
+                .map((tape) => {
+                  const currentProposal = allProposals?.filter((proposal) => (proposal.ipfs.IpfsHash === tape.proposalId ? proposal : null));
+                  if (currentProposal?.length) return <LandingTapeCovers tape={tape} proposal={currentProposal[0]} />;
+                })}
+            </Slider>
+          ) : (
+            <Flex minH="50vh" alignItems={'center'} justifyContent="center">
+              <Spinner h="12" w="12" size="md" />
+            </Flex>
+          )}
+        </Box>
+        <Image
+          src={hedImage}
+          opacity="50%"
+          zIndex="10"
+          top="24"
+          left="72%"
+          maxH="50rem"
+          display={{ lg: 'block', base: 'none' }}
+          position="absolute"
+          objectFit="contain"
+          style={{ filter: 'invert(0.75)' }}
+        />
       </Box>
-      <Box px={4} mx="auto">
-        <Stack alignItems={'start'}>
-          <Box maxW="7xl" mx="auto" px={{ base: '4', md: '8', lg: '12' }} py={{ base: '6', md: '8', lg: '12' }}>
-            <Stack height={{ md: '640px' }} direction={{ base: 'column', md: 'row' }} spacing={{ base: '6', md: '10' }} align="stretch">
-              <ImageWithOverlay
-                flex="1"
-                spacing={'4'}
-                objectPosition="top center"
-                title={latestHedsTape?.name}
-                url={latestHedsTape?.route}
-                src={latestHedsTape?.image}
-                alt={latestHedsTape?.name}
-              />
-              <Stack display={{ base: 'none', lg: 'flex' }} spacing={{ base: '6', md: '10' }} maxW={{ md: '400px' }}>
-                <ImageWithOverlay
-                  spacing="4"
-                  title={latestHedsTape?.id ? allHedsTapes[+latestHedsTape.id - 1]?.name : ''}
-                  src={latestHedsTape?.id ? allHedsTapes[+latestHedsTape.id - 1]?.image : ''}
-                  url={latestHedsTape?.id ? allHedsTapes[+latestHedsTape.id - 1]?.route : ''}
-                  alt={latestHedsTape?.id ? allHedsTapes[+latestHedsTape.id - 1]?.name : ''}
-                />
-                <ImageWithOverlay
-                  spacing="4"
-                  title={latestHedsTape?.id ? allHedsTapes[+latestHedsTape.id - 2]?.name : ''}
-                  src={latestHedsTape?.id ? allHedsTapes[+latestHedsTape.id - 2]?.image : ''}
-                  url={latestHedsTape?.id ? allHedsTapes[+latestHedsTape.id - 1]?.route : ''}
-                  alt={latestHedsTape?.id ? allHedsTapes[+latestHedsTape.id - 2]?.name : ''}
-                />
-              </Stack>
-            </Stack>
-          </Box>
-        </Stack>
-      </Box>
-      <Box mb={5}>
-        <Container py={{ base: '4', md: '8' }}>
-          <HStack>
-            <Divider borderColor="gray.700" w="full" />
-            <Text fontSize="md" fontWeight="semibold" whiteSpace="nowrap" letterSpacing={'widest'}>
-              ALL VOTES
-            </Text>
-            <Divider borderColor="gray.700" w="full" />
-          </HStack>
-        </Container>
-      </Box>
-      <Grid mx="auto" maxW="7xl" templateColumns={{ base: 'repeat(2, 1fr)', md: 'repeat(5, 1fr)' }} gap={2}>
-        {!isEmpty(allHedsTapes) &&
-          Object.values(allHedsTapes).map((tape, i) => {
-            return <VoteCard key={tape.contract + tape.name} tape={tape} />;
-          })}
-      </Grid>
-    </Container>
+      <LandingAllVotes allHedsTapes={allHedsTapes} />
+    </Fragment>
   );
 };
+export default VoteLandingMock;
