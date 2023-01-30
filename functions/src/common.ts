d6d9c8f9339105f0375e96df0bd91b82bf72890c
cgreer011 <camgreer01@gmail.com>
2023-01-30 15:38:22 -0500

    set up events for explore

--- functions/src/common.ts
+++ functions/src/common.ts
@@ -1,4 +1,4 @@
-import * as express from "express";
+import * as express from 'express';
 
 // custom request body type
 export interface TypedRequestBody<T> extends express.Request {
@@ -19,7 +19,6 @@ export interface Options {
 }
 
 // opeasea parsed, listing type
-
 
 // creates prompt for openai
 export const generatePrompt = (string: string) => `a pixel art album cover of a small ${string} in the center with a white background`;
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,15 @@
 // The Firebase Admin SDK to access Firestore.
-import * as admin from "firebase-admin";
+import * as admin from 'firebase-admin';
 admin.initializeApp();
 
 // TWITTER CONTROLLER
-import twitterAuth from "./servers/authentication";
+import twitterAuth from './servers/authentication';
 
 // SUBMISSIONS CONTROLLER
-import newSubmission from "./servers/submissions";
+import newSubmission from './servers/submissions';
 
 // OPENSEA CONTROLLER
-import activeListings from "./servers/opensea";
+import activeListings from './servers/opensea';
 
 exports.twitterAuth = twitterAuth;
 exports.newSubmission = newSubmission;
