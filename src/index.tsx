83824c168602d3b0c54d42533d7a7e429506f8fa
Ramtin Khoee <rahmteen@heds.io>
2023-01-30 12:46:21 -0800

    updated wagmi page and added connect kit in an attempt to fix mobile metamask bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as RematchProvider } from 'react-redux';
-import { WagmiConfig, createClient, configureChains, chain } from 'wagmi';
+import { configureChains, WagmiConfig, createClient } from 'wagmi';
+import { mainnet } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 import { infuraProvider } from 'wagmi/providers/infura';
-import { InjectedConnector } from 'wagmi/connectors/injected';
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { ConnectKitProvider, getDefaultClient } from 'connectkit';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { TapeWrapper, UserWrapper } from '@/modules/wrappers';
 import { ModalWrapper } from '@/modules/modals/components';
@@ -25,41 +24,38 @@ import '@fontsource/space-mono';
 
 ReactGA.initialize('G-EWK413GWSB');
 
-const INFURA_PROVIDER_KEY = 'fa2c8890fadd90f1516231c1831cdcff';
-const { chains, provider } = configureChains([chain.mainnet], [infuraProvider({ apiKey: INFURA_PROVIDER_KEY, priority: 0 }), publicProvider({ priority: 1 })]);
-const client = createClient({
-  connectors: [
-    window && window?.ethereum ? new InjectedConnector({ chains }) : new MetaMaskConnector({ chains }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        qrcode: true,
-      },
-    }),
-  ],
-  autoConnect: true,
-  provider,
-});
+const INFURA_PROVIDER_KEY = 'b8453c72aa7c484fb1efee0eed133fe6';
+const { provider } = configureChains([mainnet], [infuraProvider({ apiKey: INFURA_PROVIDER_KEY, priority: 0 }), publicProvider({ priority: 1 })]);
+const client = createClient(
+  getDefaultClient({
+    appName: 'heds',
+    provider,
+    chains: [mainnet],
+    autoConnect: true,
+  }),
+);
 
 const initializeLightDarkMode = <ColorModeScript initialColorMode={theme.config.initialColorMode} />;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <WagmiConfig client={client}>
-    <RematchProvider store={store}>
-      <ChakraProvider theme={theme}>
-        {initializeLightDarkMode}
-        <BrowserRouter>
-          <TapeWrapper>
-            <UserWrapper>
-              <AudioWrapper>
-                <ModalWrapper>
-                  <App />
-                </ModalWrapper>
-              </AudioWrapper>
-            </UserWrapper>
-          </TapeWrapper>
-        </BrowserRouter>
-      </ChakraProvider>
-    </RematchProvider>
+    <ConnectKitProvider>
+      <RematchProvider store={store}>
+        <ChakraProvider theme={theme}>
+          {initializeLightDarkMode}
+          <BrowserRouter>
+            <TapeWrapper>
+              <UserWrapper>
+                <AudioWrapper>
+                  <ModalWrapper>
+                    <App />
+                  </ModalWrapper>
+                </AudioWrapper>
+              </UserWrapper>
+            </TapeWrapper>
+          </BrowserRouter>
+        </ChakraProvider>
+      </RematchProvider>
+    </ConnectKitProvider>
   </WagmiConfig>,
 );
