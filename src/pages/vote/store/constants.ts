303629e1837aacd07503de60ea02606f2079948a
Ramtin Khoee <rahmteen@heds.io>
2023-01-31 17:00:44 -0800

    changed text for ht6

--- src/pages/vote/store/constants.ts
+++ src/pages/vote/store/constants.ts
@@ -13,4 +13,9 @@ export const ABOUT_VOTING =
   'The community votes on their favorite submissions. Voting power is determined from hedsTAPE(s) ownership. hedsTAPE(s) with a higher ratio of owners to tapes minted in the specific collection will have a higher voting power.';
 
 export const ABOUT_VOTING_OLD_TAPES =
-  'The curation of this tape followed a different process to what we currently use on Heds. Vote distribution for this tape is not currently available. Please visit the tape page to view it.';
+  'The curation of this tape followed a different process than what we currently use on heds. Tracks were chosen internally by heds with the help of the tape curator.';
+
+export const ABOUT_VOTING_HT6 =
+  'The curation of this tape followed a different process than normal. The community voted for their favorite visual background(s) to be featured on the tape, instead of the public audio submissions. The tracks that made it on to the tape were chose by the tape curator.';
+
+  
