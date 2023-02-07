b9b046659aa86291d8b731761eeffab04ac9f688
cgreer011 <camgreer01@gmail.com>
2023-02-07 10:45:16 -0500

    Add selector that sorts choices after results

--- src/pages/vote/components/Submissions.tsx
+++ src/pages/vote/components/Submissions.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { Dispatch, store } from '@/store';
 import { useDispatch, useSelector } from 'react-redux';
+
 
 import { Box, Grid, Skeleton, Text, VStack } from '@chakra-ui/react';
 import { SubmissionCard } from './SubmissionCard';
@@ -7,9 +10,17 @@ import { SubmissionCard } from './SubmissionCard';
 import { Choice } from 'hedsvote';
 
 export const Submissions = () => {
+  const { tape, id } = useParams();
   const dispatch = useDispatch<Dispatch>();
   const choices = useSelector(store.select.voteModel.selectProposalChoices);
   const proposal = useSelector(store.select.voteModel.selectProposal);
+  const scores = useSelector(store.select.voteModel.selectScores);
+  const currentTape = useSelector(store.select.tapesModel.selectCurrentVoteTape([tape, id]));
+  const sortedChoicesByResults = useSelector(store.select.voteModel.selectSortedChoicesByResults({choices, scores, tapeTrackIds: currentTape?.tracks}));
+
+  useEffect(() => {
+    console.log('sorted choices by results', sortedChoicesByResults);
+  }, [currentTape]);
 
   return (
     <Box mx="auto">
