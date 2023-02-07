1091714572d3064be979499c2ddefbf0346386f0
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-02-06 19:52:25 -0500

    Styling submission card

--- src/pages/vote/components/SubmissionCard.tsx
+++ src/pages/vote/components/SubmissionCard.tsx
@@ -9,7 +9,9 @@ export const SubmissionCard = ({ choice }: OwnProps) => {
   return (
     <Box border="1px" borderRadius="md">
       <Stack flexDirection="row">
-        <Image boxSize="4rem" borderRadius="md" src={choice.image} alt="Submission Image" />
+        <Box p={2}>
+          <Image boxSize="4rem" borderRadius="md" src={choice.image} alt="Submission Image" />
+        </Box>
         <Stack spacing="-0.5rem" pl="2px">
           <Text fontSize="sm">{choice.name}</Text>
           <Text fontSize="xs">{choice.artist}</Text>
