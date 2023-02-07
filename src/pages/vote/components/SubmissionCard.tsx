e3a9d33e5496bccacd494563a09ec49a949200f4
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-02-07 13:28:19 -0500

    Styling for diff submissions

--- src/pages/vote/components/SubmissionCard.tsx
+++ src/pages/vote/components/SubmissionCard.tsx
@@ -7,7 +7,7 @@ interface OwnProps {
 
 export const SubmissionCard = ({ choice }: OwnProps) => {
   return (
-    <Box border="1px" borderRadius="md">
+    <Box border="1px" borderRadius="md" borderColor="gray.800">
       <Stack flexDirection="row">
         <Box p={2}>
           <Image boxSize="3rem" borderRadius="md" src={choice.image} alt="Submission Image" />
@@ -20,3 +20,31 @@ export const SubmissionCard = ({ choice }: OwnProps) => {
     </Box>
   );
 };
+
+const Track = ({ choice }: OwnProps) => (
+  <Box border="1px" borderRadius="md" borderColor="gray.500" bg="purple.100">
+    <Stack flexDirection="row">
+      <Box p={2}>
+        <Image boxSize="3rem" borderRadius="md" src={choice.image} alt="Submission Image" />
+      </Box>
+      <Stack spacing="-0.5rem" pl="2px">
+        <Text fontSize="sm">{choice.name}</Text>
+        <Text fontSize="xs">{choice.artist}</Text>
+      </Stack>
+    </Stack>
+  </Box>
+);
+
+const SelectedSubmission = ({ choice }: OwnProps) => (
+  <Box border="1px" borderRadius="md" borderColor="gray.800">
+    <Stack flexDirection="row">
+      <Box p={2}>
+        <Image boxSize="3rem" borderRadius="md" src={choice.image} alt="Submission Image" />
+      </Box>
+      <Stack spacing="-0.5rem" pl="2px">
+        <Text fontSize="sm">{choice.name}</Text>
+        <Text fontSize="xs">{choice.artist}</Text>
+      </Stack>
+    </Stack>
+  </Box>
+);
