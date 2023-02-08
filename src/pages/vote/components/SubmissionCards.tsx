6e576ee31ba01b9cf7038fb09c7aa21c597bc088
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-02-08 14:06:16 -0500

    Updated for tapes 1-3

--- src/pages/vote/components/SubmissionCards.tsx
+++ src/pages/vote/components/SubmissionCards.tsx
@@ -1,44 +1,64 @@
 import { Box, Flex, Grid, Image, Progress, Stack, Text } from '@chakra-ui/react';
 import { SubmissionChoice } from '../store/voteModel';
-import { OLD_TAPES } from '@pages/vote/store/constants';
+import { Choice } from 'hedsvote';
+
 interface SubmissionProps {
   choices: [SubmissionChoice][];
   handleSelectedSubmission: (choice: SubmissionChoice) => void;
-  tapeId: string;
 }
 interface CardProps {
   choice: SubmissionChoice;
   handleSelectedSubmission: (choice: SubmissionChoice) => void;
 }
 
-export const SubmissionCards = ({ choices, handleSelectedSubmission, tapeId }: SubmissionProps) => {
-  const isOldTape = OLD_TAPES.includes(tapeId);
+interface OldTapeProps {
+  choices: Choice[];
+  handleSelectedSubmission: (choice: Choice) => void;
+}
+
+export const SubmissionCards = ({ choices, handleSelectedSubmission }: SubmissionProps) => {
   const [tracks, selected, submissions] = choices;
 
   return (
     <Grid pt={6} templateColumns={{ base: 'repeat(3, 1fr)', lg: 'repeat(4, 1fr)' }} gap={1} border="1px" borderRadius="md" p={2}>
-      {isOldTape ? (
-        <>
-          {choices.flat().map((choice) => (
-            <Track key={choice.name + choice.image} choice={choice} handleSelectedSubmission={handleSelectedSubmission} />
-          ))}
-        </>
-      ) : (
-        <>
-          {tracks.map((choice) => (
-            <Track key={choice.name + choice.image} choice={choice} handleSelectedSubmission={handleSelectedSubmission} />
-          ))}
-          {selected.map((choice) => (
-            <SelectedSubmission key={choice.name + choice.image} choice={choice} handleSelectedSubmission={handleSelectedSubmission} />
-          ))}
-          {submissions.map((choice) => (
-            <Submission key={choice.name + choice.image} choice={choice} handleSelectedSubmission={handleSelectedSubmission} />
-          ))}
-        </>
-      )}
+      {tracks.map((choice) => (
+        <Track key={choice.name + choice.image} choice={choice} handleSelectedSubmission={handleSelectedSubmission} />
+      ))}
+      {selected.map((choice) => (
+        <SelectedSubmission key={choice.name + choice.image} choice={choice} handleSelectedSubmission={handleSelectedSubmission} />
+      ))}
+      {submissions.map((choice) => (
+        <Submission key={choice.name + choice.image} choice={choice} handleSelectedSubmission={handleSelectedSubmission} />
+      ))}
     </Grid>
   );
 };
+
+export const OldTapeTrack = ({ choices, handleSelectedSubmission }: OldTapeProps) => (
+  <Grid pt={6} templateColumns={{ base: 'repeat(3, 1fr)', lg: 'repeat(4, 1fr)' }} gap={1} border="1px" borderRadius="md" p={2}>
+    {choices.map((choice) => (
+      <Box
+        border="1px"
+        borderRadius="md"
+        borderColor="gray.500"
+        bg="purple.100"
+        _hover={{ cursor: 'pointer' }}
+        onClick={() => handleSelectedSubmission(choice)}
+        key={choice.name + choice.image}
+      >
+        <Stack flexDirection="row">
+          <Box p={2}>
+            <Image boxSize="3rem" borderRadius="md" src={choice.image} alt="Submission Image" />
+          </Box>
+          <Flex direction="column" pl="2px">
+            <Text fontSize="sm">{choice.name}</Text>
+            <Text fontSize="xs">{choice.artist}</Text>
+          </Flex>
+        </Stack>
+      </Box>
+    ))}
+  </Grid>
+);
 
 const Track = ({ choice, handleSelectedSubmission }: CardProps) => (
   <Box border="1px" borderRadius="md" borderColor="gray.500" bg="purple.100" _hover={{ cursor: 'pointer' }} onClick={() => handleSelectedSubmission(choice)}>
