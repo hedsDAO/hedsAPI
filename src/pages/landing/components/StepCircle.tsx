4ac232ea218bc838eefdf7a9f8535f7c9ddf8a8b
Nancy K <njkousholt@gmail.com>
2023-02-09 11:30:58 -0500

    added dashed to check animation to StepCircle

--- src/pages/landing/components/StepCircle.tsx
+++ src/pages/landing/components/StepCircle.tsx
@@ -1,12 +1,53 @@
 import { Icon, SquareProps } from '@chakra-ui/react';
 import { IconCircleCheck, IconCircleDashed } from '@tabler/icons';
+import { animated, useInView } from 'react-spring';
 
-interface RadioCircleProps extends SquareProps {
-  isCompleted: boolean;
-}
+export const StepCircle = () => {
+  const [dashedRef, dashedProps] = useInView(
+    () => ({
+      from: {
+        opacity: 1,
+      },
+      to: {
+        opacity: 0,
+      },
+      config: {
+        tension: 30,
+      },
+      delay: 1000,
+    }),
+    {
+      rootMargin: '-30% 0px',
+      once: true,
+    },
+  );
+  const [checkRef, checkProps] = useInView(
+    () => ({
+      from: {
+        opacity: 0,
+      },
+      to: {
+        opacity: 1,
+      },
+      config: {
+        velocity: -1,
+        tension: 20,
+      },
+    }),
+    {
+      rootMargin: '-30% 0px',
+      once: true,
+    },
+  );
 
-export const StepCircle = (props: RadioCircleProps) => {
-  const { isCompleted } = props;
-
-  return <>{isCompleted ? <Icon as={IconCircleCheck} color="black" boxSize="8" /> : <Icon as={IconCircleDashed} boxSize="8" />}</>;
+  return (
+    <>
+      <animated.div ref={dashedRef} style={{ position: 'fixed', zIndex: 1, ...dashedProps }}>
+        <Icon as={IconCircleDashed} boxSize="8" />
+      </animated.div>
+      <animated.div ref={checkRef} style={{ position: 'fixed', zIndex: 0, ...checkProps }}>
+        <Icon as={IconCircleCheck} color="black" boxSize="8" />
+      </animated.div>
+    </>
+  );
 };
