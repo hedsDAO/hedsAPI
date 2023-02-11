b316d3ef039c1670223b978959342b7cb5ba39b6
Ramtin Khoee <rahmteen@heds.io>
2023-02-11 11:41:42 -0800

    added results profile picture + basic logic and flow for open tapes without user interaction/dispatch

--- src/pages/vote/components/CastVoteContainer.tsx
+++ src/pages/vote/components/CastVoteContainer.tsx
@@ -8,15 +8,14 @@ import { formatWallet } from '@/utils';
 
 // Models
 import { QuadraticVote } from 'hedsvote';
-import { useNavigate } from 'react-router-dom';
 
-export const VoteResults = () => {
+export const CastVoteContainer = () => {
   const votes = useSelector(store.select.voteModel.selectQuadraticVotes);
   const [isShowingAllResults, setIsShowingAllResults] = useBoolean();
 
   return (
     <>
-      {votes && (
+      {/* {votes && (
         <Stack>
           <Heading
             mt={{ base: 5, lg: 4 }}
@@ -36,30 +35,30 @@ export const VoteResults = () => {
             {isShowingAllResults ? 'show less' : 'show all'}
           </Button>
         </Stack>
-      )}
+      )} */}
     </>
   );
 };
 
 const VoterCard = ({ vote }: { vote: QuadraticVote }) => {
-  const { voter, vp } = vote;
-  const choices = useSelector(store.select.voteModel.selectProposalChoices);
-  const resultsUserData = useSelector(store.select.voteModel.selectResultsUserData);
-  const navigate = useNavigate();
-  const formatChoiceSelection = (voteObject: { [id: string]: number }) => {
-    const totalScore = Object.values(voteObject).reduce((a, b) => a + b, 0);
-    const selectedChoices = Object.keys(voteObject).map((id) => {
-      const choiceId = parseInt(id) - 1;
-      const choice = choices[choiceId];
-      const percentage = ((voteObject[id] / totalScore) * 100).toFixed(2);
-      return `${percentage}% for ${choice.name}`;
-    });
-    return selectedChoices.join(', ');
-  };
+  //   const { voter, vp } = vote;
+  //   const choices = useSelector(store.select.voteModel.selectProposalChoices);
+  //   const resultsUserData = useSelector(store.select.voteModel.selectResultsUserData);
+  //   const navigate = useNavigate();
+  //   const formatChoiceSelection = (voteObject: { [id: string]: number }) => {
+  //     const totalScore = Object.values(voteObject).reduce((a, b) => a + b, 0);
+  //     const selectedChoices = Object.keys(voteObject).map((id) => {
+  //       const choiceId = parseInt(id) - 1;
+  //       const choice = choices[choiceId];
+  //       const percentage = ((voteObject[id] / totalScore) * 100).toFixed(2);
+  //       return `${percentage}% for ${choice.name}`;
+  //     });
+  //     return selectedChoices.join(', ');
+  //   };
 
   return (
     <Box border="1px" borderColor="gray.400" borderRadius="md" px={1} bgColor="gray.50">
-      <Flex justifyContent="space-between" px={1} py={2}>
+      {/* <Flex justifyContent="space-between" px={1} py={2}>
         <Flex gap={2} alignItems={'center'}>
           <Avatar
             onClick={() => navigate(`/u/${vote.voter.toLowerCase()}`)}
@@ -79,7 +78,7 @@ const VoterCard = ({ vote }: { vote: QuadraticVote }) => {
             <InfoOutlineIcon color="gray.500" boxSize={3} />
           </Tooltip>
         </Flex>
-      </Flex>
+      </Flex> */}
     </Box>
   );
 };
--- src/pages/vote/components/Submissions.tsx
+++ src/pages/vote/components/Submissions.tsx
@@ -5,11 +5,12 @@ import { useDispatch, useSelector } from 'react-redux';
 // Components
 import { Box, Divider, Flex, Heading, Tooltip } from '@chakra-ui/react';
 import { InfoOutlineIcon } from '@chakra-ui/icons';
-import { SubmissionCards, OldTapeTrack } from './SubmissionCards';
+import { SubmissionCards, OldTapeTrack, OpenVoteCards } from './SubmissionCards';
 
 // Constants
 import { SubmissionChoice } from '../store/voteModel';
 import { OLD_TAPES, ABOUT_SUBMISSIONS } from '@pages/vote/store/constants';
+import { ProposalState } from 'hedsvote';
 
 export const Submissions = () => {
   const { tape, id } = useParams();
@@ -18,7 +19,7 @@ export const Submissions = () => {
   const scores = useSelector(store.select.voteModel.selectScores);
   const currentTape = useSelector(store.select.tapesModel.selectCurrentVoteTape([tape, id]));
   const sortedChoicesByResults = useSelector(store.select.voteModel.selectSortedChoicesByResults({ choices, scores, tapeTrackIds: currentTape?.tracks }));
-
+  const proposalState = useSelector(store.select.voteModel.selectProposalState);
   const handleSelectedSubmission = (choice: SubmissionChoice) => {
     dispatch.voteModel.setCurrentTrack(choice);
   };
@@ -32,13 +33,17 @@ export const Submissions = () => {
         <Heading className="animate__animated animate__fadeIn" fontWeight="medium" letterSpacing="widest" size={['xs', 'sm']} color={'gray.900'}>
           SUBMISSIONS
         </Heading>
-        <Tooltip label={ABOUT_SUBMISSIONS}>
-          <InfoOutlineIcon color="gray.500" />
-        </Tooltip>
+        {proposalState === ProposalState.CLOSED && (
+          <Tooltip label={ABOUT_SUBMISSIONS}>
+            <InfoOutlineIcon color="gray.500" />
+          </Tooltip>
+        )}
       </Flex>
       <Divider my={1.5} borderColor="transparent" w="full" />
       <Box border="1px" borderColor="gray.700" borderRadius="md" p={1} bgColor="gray.50">
-        {sortedChoicesByResults.length > 0 && !isOldTape ? (
+        {proposalState === ProposalState.OPEN ? (
+          <OpenVoteCards choices={choices} handleSelectedSubmission={handleSelectedSubmission} />
+        ) : sortedChoicesByResults.length > 0 && !isOldTape ? (
           <SubmissionCards choices={sortedChoicesByResults} handleSelectedSubmission={handleSelectedSubmission} />
         ) : (
           <OldTapeTrack choices={choices} handleSelectedSubmission={handleSelectedSubmission} />
