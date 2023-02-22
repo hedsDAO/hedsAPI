a834955422776d14bbe31f3dbc2223f13692b743
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-02-22 16:28:03 -0500

    quantity + tape address

--- src/modules/modals/screens/mint/MintModal/MintModal.tsx
+++ src/modules/modals/screens/mint/MintModal/MintModal.tsx
@@ -1,22 +1,31 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { EditionConfig, MintConfig, SoundClient } from '@soundxyz/sdk';
-import { contractAddresses } from '@soundxyz/sound-protocol';
-import { ContractTransaction } from 'ethers';
+
+// Components
+import { PrimaryButton, SecondaryButton } from '@/common/buttons';
+import { ModalContainer, ModalHeader } from '@/modules/modals/components';
+import { Dispatch, RootState, store } from '@/store';
+import { Flex, useBoolean, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper } from '@chakra-ui/react';
+import { IconDisc } from '@tabler/icons';
+import { MintDetails, TapeCover, TapeNameAndCurator, TransactionProgress } from '../components';
+
+// Utils
 import { useAccount, useConnect } from 'wagmi';
 import { mainnet, goerli } from 'wagmi/chains';
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
-import { PrimaryButton, SecondaryButton } from '@/common/buttons';
-import { ModalContainer, ModalHeader } from '@/modules/modals/components';
-import { Dispatch, RootState } from '@/store';
-import { Flex, useBoolean } from '@chakra-ui/react';
-import { IconDisc } from '@tabler/icons';
+import { EditionConfig, MintConfig, SoundClient } from '@soundxyz/sdk';
+import { contractAddresses } from '@soundxyz/sound-protocol';
+import { ContractTransaction } from 'ethers';
+
+// Constants
 import { MINT_MODAL_TITLE } from '../models/constants';
-import { MintDetails, TapeCover, TapeNameAndCurator, TransactionProgress } from '../components';
 
 const MintModal = () => {
+  const [value, setValue] = useState<number>(1);
   const [isMinting, setIsMinting] = useBoolean(false);
   const dispatch = useDispatch<Dispatch>();
   const { isOpen } = useSelector((state: RootState) => state.modalModel);
+  const contract = useSelector(store.select.tapesModel.selectCurrentTapeContract);
   const { isConnected } = useAccount();
   const { connectAsync } = useConnect();
   const connector = new MetaMaskConnector({
@@ -27,23 +36,25 @@ const MintModal = () => {
     const signer = await connector?.getSigner();
     console.log(signer);
     const client = SoundClient({ signer });
-    const editionAddress = '0x10c9b5B84c6F490972464946033835F098591Cfe';
+    const editionAddress = contract;
     const mintSchedule = (await client.activeMintSchedules({ editionAddress })).shift();
     console.log(mintSchedule);
     if (!mintSchedule) throw Error(`No active mint schedule available!`);
 
     // Transaction
-    const mintTransaction = await (await client.mint({
-      mintSchedule,
-      quantity,
-    })).wait();
+    const mintTransaction = await (
+      await client.mint({
+        mintSchedule,
+        quantity,
+      })
+    ).wait();
     console.log(await mintTransaction);
     return mintTransaction;
   };
 
-  const handleMintStatus = async (quantity: number) => {
+  const handleMintStatus = async () => {
     setIsMinting.on();
-     await mintEdition(quantity);
+    await mintEdition(value);
     setIsMinting.off();
   };
 
@@ -54,10 +65,19 @@ const MintModal = () => {
       <TapeCover />
       <MintDetails />
       {/* TODO: add progress states for mint status */}
-      <Flex gap={2} w="full">
+      <Flex justifyContent="space-between">
         <SecondaryButton onClick={() => dispatch.modalModel.setModalOpen(false)}>Back</SecondaryButton>
-        <PrimaryButton onClick={handleMintStatus}>Mint</PrimaryButton>
-        {isMinting && <TransactionProgress />}
+        <Flex gap={3} alignItems="center">
+          {isMinting && <TransactionProgress />}
+          <NumberInput min={1} max={5} value={value.toString()} onChange={(val) => setValue(parseInt(val))} maxW={20} size="sm">
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+          <PrimaryButton onClick={handleMintStatus}>Mint</PrimaryButton>
+        </Flex>
       </Flex>
     </ModalContainer>
   );
--- src/modules/modals/screens/mint/components/TransactionProgress/TransactionProgress.tsx
+++ src/modules/modals/screens/mint/components/TransactionProgress/TransactionProgress.tsx
@@ -4,8 +4,8 @@ import { Spinner } from '@chakra-ui/react';
 const TransactionProgress = () => {
   return (
     <>
-      <Spinner thickness="3px" speed="0.5s" emptyColor="gray.300" color="blackAlpha.500" size="lg" />
-      <LabelBadge label="status" text="processing" textColor={'orange.500'} />
+      <Spinner thickness="3px" speed="0.5s" emptyColor="gray.300" color="blackAlpha.500" size="md" />
+      {/* <LabelBadge label="status" text="processing" textColor={'orange.500'} /> */}
     </>
   );
 };
