eef2fadfc239efaee2a78f8f764972e24771763b
cgreer011 <camgreer01@gmail.com>
2023-03-01 17:22:34 -0500

    Add share tweet button to mint modal

--- src/modules/modals/screens/mint/models/constants.ts
+++ src/modules/modals/screens/mint/models/constants.ts
@@ -18,6 +18,19 @@ export const PRICE_LABEL = 'Price';
 export const PRICE_VALUE = '0.1 ETH';
 export const MINTED_LABEL = 'Minted';
 
+/*
+ * @const URL : Twitter URL
+ * @const TARGET : Browsing context
+ * @const SIZE : Window size
+ * @const COLLECTED_TWEET: Tweet to display;
+ * @const COLLECT_PAGE_LINK: Link to display;
+ */
+export const URL = 'https://twitter.com/intent/tweet?text=';
+export const TARGET = 'mywin';
+export const SIZE = 'width=700,height=700';
+export const COLLECTED_TWEET = 'just collected hedsTAPE';
+export const COLLECT_PAGE_LINK = 'https://www.heds.app/listen/';
+
 export const ERC_TOKEN_DESCRIPTION = 'This is an ERC-721 token';
 export const LANYARD_API = 'https://lanyard.org/api/v1/tree?root='
 export const SOUND_KEY = '3ca9ceee-35f2-4db0-8277-fc1fc553484a'
