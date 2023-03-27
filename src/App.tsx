66882c84aed933f665f1125e73d534b732d9c0d9
Ramtin Khoee <rahmteen@heds.io>
2023-03-27 09:15:22 -0700

    added mvp for song comp

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { getStorage } from 'firebase/storage';
 
 import firebaseConfig from './firebaseConfig';
 
+import { Song } from './common/song/Song';
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
 const App = (): JSX.Element => {
-  return <Routes></Routes>;
+  return <Song />;
 };
 
 export default App;
