05ba4220a12c5084bbb8ac2bdbaad4864e46e577
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-03-29 23:17:58 +0800

    Typed BE responses

--- src/pages/song/store/songModel.ts
+++ src/pages/song/store/songModel.ts
@@ -26,13 +26,29 @@ export enum TrackType {
   HEDSOLO,
 }
 
+export interface SongResponse {
+  id: number;
+  audio: string;
+  cover: string;
+  duration: number;
+  public: boolean;
+  appear_on?: number[];
+  track_name: string;
+  type: TrackType;
+  submission_data: string;
+  cyanite_id: string;
+  created: number;
+  total_likes: number;
+}
+
 export interface SongState {
+  id: number | undefined;
   audio: string;
   contributors: Contributor[];
   cover: string;
   duration: number;
   public: boolean;
-  stats: TrackStats;
+  stats?: TrackStats; // are we removing this?
   appearsOn: string[];
   trackName: string;
   type: TrackType;
@@ -43,6 +59,7 @@ export interface SongState {
 
 export const songModel = createModel<RootModel>()({
   state: {
+    id: undefined,
     audio: '',
     contributors: [],
     cover: 'string',
--- src/pages/user/store/userModel.ts
+++ src/pages/user/store/userModel.ts
@@ -1,25 +1,11 @@
-export interface UserState {
-  badges: Badge[];
-  banner: string;
-  collection: Collection;
-  description: string;
-  displayName: string;
-  history?: History;
-  joined: number;
-  likes?: string[];
-  profilePicture: string;
-  votes?: Vote; // should be object
-  wallet: string;
-  spotlight?: string;
-  role: Role;
-}
+import { createModel } from '@rematch/core';
+import type { RootModel } from '@/models';
 
 interface Badge {
   description: string;
   image: string;
   name: string; // can change to enum
 }
-
 interface Collection {
   lastUpdated: number;
   items: CollectionItem[];
@@ -45,3 +31,54 @@ enum Role {
   ARTIST,
   ADMIN,
 }
+
+export interface UserResponse {
+  id: number;
+  badges: string;
+  banner: string;
+  collection: string;
+  description: string;
+  display_name: string;
+  history: string;
+  joined: number;
+  likes?: number;
+  profile_picture: string;
+  songs: number[];
+  votes?: string;
+  wallet: string;
+  spotlight?: string;
+  role: Role;
+}
+
+export interface UserState {
+  id: number | undefined;
+  badges: Badge[];
+  banner: string;
+  collection: Collection | undefined;
+  description: string;
+  displayName: string;
+  history?: History;
+  joined: number;
+  likes?: number[];
+  profilePicture: string;
+  songs?: number[];
+  votes?: Vote; // should be object
+  wallet: string;
+  spotlight?: string;
+  role: Role | undefined;
+}
+
+export const userModel = createModel<RootModel>()({
+  state: {
+    id: undefined,
+    badges: [],
+    banner: '',
+    collection: undefined,
+    description: '',
+    displayName: '',
+    joined: undefined,
+    profilePicture: '',
+    wallet: '',
+    role: undefined,
+  },
+});
