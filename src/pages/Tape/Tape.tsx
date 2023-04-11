d85e3e1ea266eb21123e2830c65b9a7e7379ab36
Ramtin Khoee <rahmteen@heds.io>
2023-04-11 09:54:15 -0700

    updated staging

--- src/pages/Tape/Tape.tsx
+++ src/pages/Tape/Tape.tsx
@@ -1,24 +1,4 @@
-import { mockTape, mockUser, tabs } from './models/constant';
-import { TapeDetails, TapeHeader, TapeTracks } from './components';
-import { Tape as TapeMetadata, User } from '@/models/common';
-import { Box, Divider } from '@chakra-ui/react';
-import { useSelector } from 'react-redux';
-import { store } from '@/store';
-import { TapeNavbarTabs } from './models/common';
-import { Nav } from '@/components/Nav/Nav';
 
 export const Tape = () => {
-  const tape = mockTape as TapeMetadata;
-  const user = mockUser as User;
-  const currentTab = useSelector(store.select.navModel.selectCurrentTab);
-
-  return (
-    <Box>
-      <TapeHeader tape={tape} curator={user} />
-      <Nav tabs={tabs} />
-      <Divider/>
-      {currentTab === TapeNavbarTabs.TRACKS && <TapeTracks />}
-      {currentTab === TapeNavbarTabs.DETAILS && <TapeDetails tape={tape} curator={user} />}
-    </Box>
-  );
+  return <></>;
 };
--- src/pages/Tape/components/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as TapeHeader } from './TapeHeader';
-export { default as TapeTracks } from './TapeTracks';
-export { default as TapeDetails } from './TapeDetails';
