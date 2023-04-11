d32b304682d49d904a26646cf6a5692fb4581024
Ramtin Khoee <rahmteen@heds.io>
2023-04-11 09:54:15 -0700

    updated staging

--- src/pages/Song/components/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as SongDetails } from './SongDetails';
-export { default as SongHeader } from './SongHeader';
-export { default as SongWaveform } from './SongWaveform';
-export { default as SongAppearsOn } from './SongAppearsOn';
-export { default as SongLikes } from './SongLikes';
-export { default as SongInsights } from './SongInsights';
