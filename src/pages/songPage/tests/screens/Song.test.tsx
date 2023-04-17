d47b89c6d37b2a8da854e6772b13fb4ba345d81e
Ramtin Khoee <rahmteen@heds.io>
2023-04-17 11:36:07 -0700

    rename fix

--- src/pages/songPage/tests/screens/Song.test.tsx
+++ src/pages/songPage/tests/screens/Song.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { Song } from '@pages/song/screens/Song';
+import { Song } from '@/pages/songPage/screens/Song';
 
 describe('Song Page', () => {
   test('renders the title', () => {
