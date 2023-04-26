01d995fed88591f074ac4658450b2313fe457e75
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-04-26 13:28:47 -0400

    styled FAQ

---
+++ src/pages/faq/screens/styles.ts
@@ -0,0 +1,12 @@
+import { StackProps, HeadingProps } from '@chakra-ui/react';
+
+export const $stackStyles: StackProps = {
+  spacing: 6,
+  width: '100%',
+  mt: 12,
+};
+
+export const $headingStyles: HeadingProps = {
+  size: 'xl',
+  color: 'white',
+};
