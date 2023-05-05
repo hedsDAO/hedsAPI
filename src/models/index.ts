d6603aa812765b1006968a4bef3174aee62952b7
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-05-05 18:12:38 -0400

    tape page WIP

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,7 @@ import { Models } from '@rematch/core';
 import { songModel } from '@/pages/song/models/songModel';
 import { modalModel } from '@/modals/models/modalModel';
 import { authModel } from '@/auth/models/authModel';
+import { tapeModel } from '@/pages/tape/models/tapeModel';
 import { userModel } from '@/pages/user/models/userModel';
 import { navModel } from '@/components/Nav/models/navModel';
 import { paginationModel } from '@/components/Pagination/models/paginationModel';
@@ -12,6 +13,7 @@ export interface RootModel extends Models<RootModel> {
   songModel: typeof songModel;
   modalModel: typeof modalModel;
   authModel: typeof authModel;
+  tapeModel: typeof tapeModel;
   userModel: typeof userModel;
   navModel: typeof navModel;
   paginationModel: typeof paginationModel;
@@ -23,6 +25,7 @@ export const models: RootModel = {
   songModel,
   modalModel,
   authModel,
+  tapeModel,
   userModel,
   navModel,
   paginationModel,
