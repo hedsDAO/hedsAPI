4078d26671fc550a40fcc7f4c1d64a9a56eed41f
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-05-16 12:01:10 -1000

    Mint Modal WIP

--- src/modals/screens/mint/Mint.tsx
+++ src/modals/screens/mint/Mint.tsx
@@ -1,9 +1,12 @@
+import { useSelector } from 'react-redux';
 import {
   Avatar,
   Box,
   Button,
   Checkbox,
   Flex,
+  Image,
+  Select,
   Stack,
   Text,
   Modal,
@@ -16,25 +19,50 @@ import {
   useDisclosure,
 } from '@chakra-ui/react';
 
+import { Dispatch, store } from '@/store';
+
 export const Mint = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const cover = useSelector(store.select.tapeModel.selectTapeCover);
+  const sampleArtists = useSelector(store.select.tapeModel.selectSampleArtists);
+
   return (
     <>
       <Button onClick={onOpen}>Open Modal</Button>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={true} onClose={onClose} size="lg">
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>boobs </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant="ghost">Secondary Action</Button>
-          </ModalFooter>
+          <ModalCloseButton color="white" />
+          <ModalBody p={6}>
+            <Text color="#DC89FF">MINT:</Text>
+            <Stack direction="row" spacing={4}>
+              <Stack direction="column" spacing={3} w="40%">
+                {sampleArtists.map((artist) => (
+                  <Box>
+                    <Avatar size="sm" src={artist.profile_picture} />
+                    <Text color="white">{artist.display_name}</Text>
+                  </Box>
+                ))}
+                <Box display="flex" flexDirection="row" justifyContent="space-around" bgColor="#26232D" p={3} borderRadius="md">
+                  <Text color="white">PRICE</Text>
+                  <Text color="#DC89FF">0.005</Text>
+                </Box>
+                <Box display="flex" flexDirection="row" justifyContent="space-around" bgColor="#26232D" p={3} borderRadius="md">
+                  <Text color="white">MINTED</Text>
+                  <Text color="#DC89FF">100</Text>
+                </Box>
+                <Select bgColor="#26232D" variant="filled" color="white" placeholder="Quantity">
+                  <option value="option1">1</option>
+                  <option value="option2">2</option>
+                  <option value="option3">3</option>
+                </Select>
+                <Button bgColor="#745CBA" color="white">
+                  Mint
+                </Button>
+              </Stack>
+              <Image src={cover} alt="tape-cover" boxSize="2xs" border="1px" borderColor="heds.400" />
+            </Stack>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
