fa81d6c5f0938722f3310ad8b4e3cdb008794c65
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-05-22 10:18:54 -1000

    syntax for query

--- functions/src/controllers/tapes/index.ts
+++ functions/src/controllers/tapes/index.ts
@@ -57,7 +57,7 @@ export const getTapeSongs = async (tape_id: number): Promise<any> => {
       s.*,
       u.id as artist_id,
       u.display_name as artist_display_name,
-      u.profile_picture as artist_profile_picture
+      u.profile_picture as artist_profile_picture,
       u.wallet as artist_wallet
     FROM ${schemaName}.songs s
     INNER JOIN ${schemaName}.song_artists sa ON s.id = sa.song_id
