5d709708440da1ae5b78535ca80ff1253767fb40
Ramtin Khoee <rahmteen@heds.io>
2023-05-23 12:15:03 -0700

    updated empty states for user pages likes/discography/collection data

--- src/pages/user/components/Collection/styles.ts
+++ src/pages/user/components/Collection/styles.ts
@@ -1,4 +1,4 @@
-import { SimpleGridProps, GridItemProps } from '@chakra-ui/react';
+import { SimpleGridProps, GridItemProps, BoxProps, TextProps } from '@chakra-ui/react';
 
 /**
  * @constant {GridItemProps} $mainContainerStyles
@@ -24,4 +24,26 @@ export const $paginationContainerStyles: GridItemProps = {
 export const $collectionGridStyles: SimpleGridProps = {
   gap: 5,
   columns: { base: 2, md: 4, xl: 4 },
+};
+
+export const $emptyBoxStyles: BoxProps = {
+  h: 'full',
+  w: 'full',
+};
+
+export const $emptyTextStyles: TextProps = {
+  minH: '3ch',
+}
+
+export const $gridItemStyles: GridItemProps = {
+  colSpan: 1,
+};
+
+export const $emptyCollectionBoxStyles:BoxProps ={
+  border: 'solid 1px',
+  rounded: 'lg',
+  px: 3,
+  pt: 3,
+  pb: 2,
+  bg: 'heds.bg3',
 };
