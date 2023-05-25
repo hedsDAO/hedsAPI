feb4d3b0a784e3de28148fe9cb4c5d22123bbe7d
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-05-24 14:28:32 -1000

    Styling code cleanup

--- src/pages/vote/components/Submission/Submissions.tsx
+++ src/pages/vote/components/Submission/Submissions.tsx
@@ -1,93 +1,50 @@
-import { Box, Flex, Grid, Image, Progress, Stack, Text } from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
+import { Box, Flex, Grid, Image, Progress, Stack, Skeleton, Text } from '@chakra-ui/react';
 import { ChoiceWithScore } from '@/pages/vote/models/voteModel';
+import { store } from '@/store';
+import * as styles from '@/pages/vote/components/Submission/styles';
 
 export const Submissions = ({ choices }: { choices: ChoiceWithScore[][] }) => {
   const [tracks, selected, submissions] = choices;
+  const isLoading = useSelector(store.select.voteModel.selectIsLoading);
   return (
-    <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(4, 1fr)' }} gap={1} pt={4}>
-      {/* <Flex flexWrap="wrap" mx={{ base: 2, lg: 8 }} px={['20px', '60px', '100px', '120px']}> */}
-      {tracks.map((choice) => (
-        <Track key={choice.name + choice.image} choice={choice} />
-      ))}
-      {selected.map((choice) => (
-        <SelectedSubmission key={choice.name + choice.image} choice={choice} />
-      ))}
-      {submissions.map((choice) => (
-        <SubmissionCard key={choice.name + choice.image} choice={choice} />
-      ))}
-      {/* </Flex> */}
-    </Grid>
+    <Skeleton isLoaded={!isLoading}>
+      <Grid {...styles.$submissionsGridStyles}>
+        {tracks.map((choice) => (
+          <Submission key={choice.name + choice.image} {...choice} $submissionType={'track'} />
+        ))}
+        {selected.map((choice) => (
+          <Submission key={choice.name + choice.image} {...choice} $submissionType={'selected'} />
+        ))}
+        {submissions.map((choice) => (
+          <Submission key={choice.name + choice.image} {...choice} $submissionType={'submission'} />
+        ))}
+      </Grid>
+    </Skeleton>
   );
 };
 
-const Track = ({ choice }: { choice: ChoiceWithScore }) => (
-  <Box border="1px" borderRadius="md" borderColor="#9293FF" bg="#745CBA" _hover={{ cursor: 'pointer' }}>
-    <Stack flexDirection="row">
-      <Box p={2}>
-        <Image minW="3rem" minH="3rem" boxSize="3rem" borderRadius="md" src={choice.image} alt="Submission Image" />
-      </Box>
-      <Flex w="full" direction="column" pl={1} pr={2}>
-        <Text mt={'-1px !important'} fontSize="xs" color="#DFDFDF" fontFamily="monospace">
-          {choice.name}
-        </Text>
-        <Flex mt={-0.5} minW="full" justifyContent={'space-between'}>
-          <Text fontSize="2xs" textColor="#FFFFFF" fontFamily="inter" letterSpacing="widest">
-            {choice.artist}
-          </Text>
-          <Text mt={1} fontSize="2xs" textColor="white">
-            {+choice.score.toFixed(2)}%
-          </Text>
-        </Flex>
-        <Progress mt={1} size="sm" value={choice.score} colorScheme="gray" borderRadius="md" />
-      </Flex>
-    </Stack>
-  </Box>
-);
+interface OwnProps extends ChoiceWithScore {
+  $submissionType: string;
+}
 
-const SelectedSubmission = ({ choice }: { choice: ChoiceWithScore }) => (
-  <Box border="1px" borderRadius="md" borderColor="#9293FF" bg="#4F4F4F" _hover={{ cursor: 'pointer' }}>
-    <Stack flexDirection="row">
-      <Box p={2}>
-        <Image minW="3rem" minH="3rem" boxSize="3rem" borderRadius="md" src={choice.image} alt="Submission Image" />
-      </Box>
-      <Flex w="full" direction="column" pl={1} pr={2}>
-        <Text mt={'-1px !important'} fontSize="xs" color="#DFDFDF" fontFamily="monospace">
-          {choice.name}
-        </Text>
-        <Flex mt={-0.5} minW="full" justifyContent={'space-between'}>
-          <Text fontSize="2xs" textColor="#FFFFFF" fontFamily="inter" letterSpacing="widest">
-            {choice.artist}
-          </Text>
-          <Text mt={1} fontSize="2xs" textColor="white">
-            {+choice.score.toFixed(2)}%
-          </Text>
+const Submission = (props: OwnProps) => {
+  const { image, name, artist, score, $submissionType } = props;
+  return (
+    <Box {...styles.$submissionBoxStyles($submissionType)}>
+      <Stack {...styles.$cardStackStyles}>
+        <Box {...styles.$cardImageBoxStyles}>
+          <Image {...styles.$cardImageStyles} src={image} alt="Submission Image" />
+        </Box>
+        <Flex {...styles.$cardFlexStyles}>
+          <Text {...styles.$cardTextStyles.name}>{name}</Text>
+          <Flex {...styles.$cardInnerFlexStyles}>
+            <Text {...styles.$cardTextStyles.artist}>{artist}</Text>
+            <Text {...styles.$cardTextStyles.score}>{+score.toFixed(2)}%</Text>
+          </Flex>
+          <Progress value={score} {...styles.$cardProgressStyles} />
         </Flex>
-        <Progress mt={1} size="sm" value={choice.score} colorScheme="gray" borderRadius="md" />
-      </Flex>
-    </Stack>
-  </Box>
-);
-
-const SubmissionCard = ({ choice }: { choice: ChoiceWithScore }) => (
-  <Box border="1px" borderRadius="md" borderColor="#9293FF" _hover={{ cursor: 'pointer' }}>
-    <Stack flexDirection="row">
-      <Box p={2}>
-        <Image minW="3rem" minH="3rem" boxSize="3rem" borderRadius="md" src={choice.image} alt="Submission Image" />
-      </Box>
-      <Flex w="full" direction="column" pl={1} pr={2}>
-        <Text mt={'-1px !important'} fontSize="xs" color="#DFDFDF" fontFamily="monospace">
-          {choice.name}
-        </Text>
-        <Flex mt={-0.5} minW="full" justifyContent={'space-between'}>
-          <Text fontSize="2xs" textColor="#FFFFFF" fontFamily="inter" letterSpacing="widest">
-            {choice.artist}
-          </Text>
-          <Text mt={1} fontSize="2xs" textColor="white">
-            {+choice.score.toFixed(2)}%
-          </Text>
-        </Flex>
-        <Progress mt={1} size="sm" value={choice.score} colorScheme="gray" borderRadius="md" />
-      </Flex>
-    </Stack>
-  </Box>
-);
+      </Stack>
+    </Box>
+  );
+};
