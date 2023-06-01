aff98da3700c4d5ea722c06574e8d27be669fa18
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-06-01 18:47:05 -0400

    Hover state, rendering artist pic, tape url

--- src/common/media/CollectionItem.tsx
+++ src/common/media/CollectionItem.tsx
@@ -6,7 +6,7 @@ export const CollectionItem = ({ item }: { item: TapeCollectionItem }) => {
   const [hasImageLoaded, setHasImageLoaded] = useBoolean();
   return (
     <Box data-testid="collection-item" border="solid 1px" borderColor="heds.100" rounded={'md'} px={3} pt={3} pb={2} bg="heds.bg2">
-      <Link to={`/t/${item.id}`}>
+      <Link to={`/tape/${item.id}`}>
         <AspectRatio ratio={1}>
           <Skeleton startColor="heds.bg2" endColor="heds.400" isLoaded={hasImageLoaded} fitContent rounded={'md'}>
             <Image onLoad={setHasImageLoaded.on} border="solid 1px" borderColor="heds.100" rounded={'md'} src={item.image} />
