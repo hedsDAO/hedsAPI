5ac1c846216c48d89bebfab64dea2ce0217500f9
Ramtin Khoee <rahmteen@heds.io>
2023-06-05 10:21:32 -0700

    updated pagination on prof page

--- src/pages/user/components/Discography/Discography.tsx
+++ src/pages/user/components/Discography/Discography.tsx
@@ -18,13 +18,18 @@ export const Discography = () => {
   const itemsPerPage = useSelector(store.select.paginationModel.selectItemsPerPage);
   const start = currentPage * itemsPerPage;
   const end = start + itemsPerPage;
+
+  const handleLikeEmptyStates = () => {
+    const itemsOnCurrentPage = songs?.slice(start, end)?.length;
+    return songs?.length < 4 ? 4 - songs?.length : itemsOnCurrentPage < 4 ? 4 - itemsOnCurrentPage : 0;
+  };
   return (
     <GridItem {...styles.$mainContainerStyles}>
       <SimpleGrid {...styles.$discographyGridStyles}>
         {songs?.length
           ? songs?.slice(start, end).map((item) => (
               <GridItem key={item.id + item.cover}>
-                <TrackItem link={item.audio.split('/ipfs/')[1]} name={item.submission_data.sub_id} image={item.cover} />
+                <TrackItem link={item.audio.split('/ipfs/')[1]} name={item.track_name} image={item.cover} />
               </GridItem>
             ))
           : [0, 1, 2, 3].map((_, index: number) => (
@@ -37,6 +42,17 @@ export const Discography = () => {
                 </Box>
               </GridItem>
             ))}
+        {songs?.length > 0 &&
+          Array.from(Array(handleLikeEmptyStates()).keys()).map((i: number) => (
+            <GridItem {...styles.$gridItemStyles} key={i + 'discog-empty'}>
+              <Box {...styles.$emptyCollectionBoxStyles}>
+                <AspectRatio ratio={1}>
+                  <Box {...styles.$emptyBoxStyles} />
+                </AspectRatio>
+                <Text {...styles.$emptyTextStyles} />
+              </Box>
+            </GridItem>
+          ))}
       </SimpleGrid>
       {numOfSongs > 4 && (
         <GridItem {...styles.$paginationContainerStyles}>
