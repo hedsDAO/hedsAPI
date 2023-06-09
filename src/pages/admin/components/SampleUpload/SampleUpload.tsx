d4f1ed90509fca21b4a58b7dbe934623c1e0a535
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-06-09 16:36:55 -0400

    Stepper with fields WIP

--- src/pages/admin/components/SampleUpload/SampleUpload.tsx
+++ src/pages/admin/components/SampleUpload/SampleUpload.tsx
@@ -1,12 +1,19 @@
 import { useRef } from 'react';
-import { Box, Button, Input } from '@chakra-ui/react';
-export const SampleUpload = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
+import { Box, Button, FormControl, FormLabel, Flex, Input } from '@chakra-ui/react';
 
+export const SampleUpload = ({ goToPrevious }: { goToPrevious: () => void }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <Box maxW="sm">
-      <Button onClick={() => inputRef.current?.click()}>Upload sample</Button>
-      <Input ref={inputRef} type="file" accept=".mp3,audio/*" hidden color="white" />
+    <Box maxW="md" mx="auto" mt={12}>
+      <FormControl>
+        <FormLabel color="white">Upload sample</FormLabel>
+        <Button onClick={() => inputRef.current?.click()}>Choose file</Button>
+        <Input ref={inputRef} type="file" accept=".mp3,audio/*" hidden color="white" />
+      </FormControl>
+      <Flex justifyContent="space-between" maxW="lg" mt={12} mx="auto">
+        <Button onClick={goToPrevious}>back</Button>
+        <Button>submit</Button>
+      </Flex>
     </Box>
   );
 };
