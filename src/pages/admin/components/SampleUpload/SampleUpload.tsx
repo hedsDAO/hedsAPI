0ad1eb1e8444f3f207e859d8febc091912669948
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-06-14 19:29:42 -0400

    WIP

--- src/pages/admin/components/SampleUpload/SampleUpload.tsx
+++ src/pages/admin/components/SampleUpload/SampleUpload.tsx
@@ -1,8 +1,8 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { Box, Button, FormControl, FormLabel, Flex, Input } from '@chakra-ui/react';
-
-export const SampleUpload = ({ goToPrevious, handleSubmit }: { goToPrevious: () => void; handleSubmit: (sample: File) => void }) => {
-  const [sample, setSample] = useState<File>(undefined);
+import { storage } from '@/App';
+import { ref, uploadBytes } from 'firebase/storage';
+export const SampleUpload = ({ goToPrevious, handleSubmit }: { goToPrevious: () => void; handleSubmit: () => void }) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   return (
@@ -10,11 +10,18 @@ export const SampleUpload = ({ goToPrevious, handleSubmit }: { goToPrevious: () 
       <FormControl>
         <FormLabel color="white">Upload sample</FormLabel>
         <Button onClick={() => inputRef.current?.click()}>Choose file</Button>
-        <Input ref={inputRef} type="file" accept=".mp3,audio/*" hidden color="white" onChange={(e) => setSample(e.target.files[0])} />
+        <Input
+          ref={inputRef}
+          type="file"
+          accept=".mp3,audio/*"
+          hidden
+          color="white"
+          onChange={(e) => uploadBytes(ref(storage, 'sample-audio.mp3'), e.target.files[0])}
+        />
       </FormControl>
       <Flex justifyContent="space-between" maxW="lg" mt={12} mx="auto">
         <Button onClick={goToPrevious}>back</Button>
-        <Button onClick={() => handleSubmit(sample)}>submit</Button>
+        <Button onClick={handleSubmit}>submit</Button>
       </Flex>
     </Box>
   );
--- src/pages/admin/model/adminModel.ts
+++ src/pages/admin/model/adminModel.ts
@@ -1,2 +1,44 @@
 import type { RootModel } from '@/models';
 import { createModel } from '@rematch/core';
+import { storage } from '@/App';
+import { ref, uploadBytes } from 'firebase/storage';
+
+interface AdminState {
+  coverImage: string;
+  sampleAudio: string;
+}
+
+export const adminModel = createModel<RootModel>()({
+  state: {} as AdminState,
+  reducers: {
+    setCoverImage: (state, payload) => ({ ...state, coverImage: payload }),
+    setSampleAudio: (state, payload) => ({ ...state, sampleAudio: payload }),
+  },
+  selectors: (slice) => ({
+    selectAdmins: () => slice((state) => state),
+  }),
+  effects: (dispatch) => ({
+    async uploadCoverImage(file: File) {
+      try {
+        await uploadBytes(ref(storage, 'cover-img.png'), file).then((snapshot) => {
+          console.log('snapshot', snapshot);
+          this.setCoverImage(snapshot.ref.fullPath);
+        });
+      } catch (error: any) {
+        console.log(error);
+      }
+    },
+  }),
+});
+
+// export const uploadFile = async (file: File) => {
+//   await uploadBytes(ref(storage, 'cover-img.png'), file).then((snapshot) => {
+//     console.log('snapshot', snapshot);
+//   });
+// };
+
+// export const uploadSample = async (file: File) => {
+//   await uploadBytes(ref(storage, 'sample-audio.mp3'), file).then((snapshot) => {
+//     console.log('snapshot', snapshot);
+//   });
+// };
