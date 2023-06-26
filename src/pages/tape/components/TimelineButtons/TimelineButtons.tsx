6f7c0057930a073952fc6f427104830d6f8645e1
cgreer011 <camgreer01@gmail.com>
2023-06-26 18:04:03 -0400

    revert changes

--- src/pages/tape/components/TimelineButtons/TimelineButtons.tsx
+++ src/pages/tape/components/TimelineButtons/TimelineButtons.tsx
@@ -18,7 +18,6 @@ export const TimelineButtons = () => {
   const dispatch = useDispatch<Dispatch>();
   const timeline = useSelector(store.select.tapeModel.selectTimeline);
   const cycle = useSelector(store.select.tapeModel.selectCurrentCycle);
-  const proposalId = useSelector(store.select.tapeModel.selectTapeProposalId);
 
   const formatTime = (time: number) => {
     if (time !== 0) {
@@ -68,7 +67,7 @@ export const TimelineButtons = () => {
             )}
             <Text {...styles.$cycleTimeTextStyles}>{formatTime(timeline?.vote?.start)}</Text>
           </HStack>
-          <Button {...styles.$buttonStyles} leftIcon={<i className="fa-sharp fa-solid fa-circle-check"></i>} onClick={() => navigate(`/vote/${proposalId}`)}>
+          <Button {...styles.$buttonStyles} leftIcon={<i className="fa-sharp fa-solid fa-circle-check"></i>} onClick={() => navigate(`/vote/${id}`)}>
             {cycle === 'vote' ? 'VOTE NOW' : 'RESULTS'}
           </Button>
         </>
