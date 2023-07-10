b1435efe6beec3c7a8b030a8479537d9ed9dc197
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-07-10 16:14:54 -0400

    Code cleanup WIP

--- src/pages/admin/components/TapeDetailsForm/TapeDetailsForm.tsx
+++ src/pages/admin/components/TapeDetailsForm/TapeDetailsForm.tsx
@@ -1,12 +1,13 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Dispatch } from '@/store';
 
-import { Box, Button, Stack, Select, Flex, FormControl, FormLabel, Icon, Input, Text } from '@chakra-ui/react';
-import { AddIcon, ArrowForwardIcon } from '@chakra-ui/icons';
+import { Box, Stack, Flex } from '@chakra-ui/react';
+import { CustomUpload } from '@pages/admin/components/CustomUpload/CustomUpload';
+import { CustomFormInput } from '@pages/admin/components/CustomFormInput/CustomFormInput';
+import { NextStepButton } from '@pages/admin/components/NextStepButton/NextStepButton';
 
 export const TapeDetailsForm = ({ goToNext }: { goToNext: () => void }) => {
-  const inputRef = useRef<HTMLInputElement>(null);
   const dispatch = useDispatch<Dispatch>();
 
   const [fileName, setFileName] = useState<string>('');
@@ -35,112 +36,28 @@ export const TapeDetailsForm = ({ goToNext }: { goToNext: () => void }) => {
   return (
     <Box w="full" mt={6}>
       <Stack spacing={5} pl={12}>
-        <FormControl>
-          <FormLabel color="gray.200" fontFamily="mono" fontWeight="semibold" fontSize="sm" letterSpacing="tight">
-            Upload Cover
-          </FormLabel>
-          <Box
-            border="2px dashed"
-            borderColor="gray.200"
-            borderRadius="lg"
-            padding="1em"
-            width="200px"
-            height="200px"
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            textAlign="center"
-          >
-            <Input type="file" id="upload" accept="image/*" onChange={handleFileChange} hidden />
-            <Button as="label" htmlFor="upload" borderRadius="full" padding="1em" variant="unstyled" cursor="pointer" onClick={() => inputRef.current?.click()}>
-              <Icon as={AddIcon} boxSize={6} color="gray.200" />
-            </Button>
-            <Text marginTop="1em" fontSize="sm" isTruncated color="gray.400" flexWrap="wrap">
-              {fileName ? fileName : 'no file selected'}
-            </Text>
-          </Box>
-        </FormControl>
-        <FormControl>
-          <FormLabel color="gray.200" fontFamily="mono" fontWeight="semibold" fontSize="sm" letterSpacing="tight">
-            Title
-          </FormLabel>
-          <Input
-            placeholder="what's the title?"
-            variant="flushed"
-            borderTop="none"
-            borderLeft="none"
-            borderRight="none"
-            borderColor="gray.400"
-            color="white"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel color="gray.200" fontFamily="mono" fontWeight="semibold" fontSize="sm" letterSpacing="tight">
-            Description
-          </FormLabel>
-          <Input
-            placeholder="write a description..."
-            variant="flushed"
-            borderTop="none"
-            borderLeft="none"
-            borderRight="none"
-            borderColor="gray.400"
-            color="white"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel color="gray.200" fontFamily="mono" fontWeight="semibold" fontSize="sm" letterSpacing="tight">
-            BPM
-          </FormLabel>
-          <Input
-            placeholder="tempo?"
-            variant="flushed"
-            borderTop="none"
-            borderLeft="none"
-            borderRight="none"
-            borderColor="gray.400"
-            color="white"
-            type="number"
-            value={bpm}
-            onChange={(e) => setBpm(e.target.value)}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel color="gray.200" fontFamily="mono" fontWeight="semibold" fontSize="sm" letterSpacing="tight">
-            Type of Tape
-          </FormLabel>
-          <Select
-            bg="white"
-            color="gray.800"
-            borderRadius="xl"
-            w="fit-content"
-            placeholder="Select tape type"
-            value={tapeType}
-            onChange={(e) => setTapeType(e.target.value)}
-          >
-            <option value="hedstape">hedsTAPE</option>
-            <option value="collabtape">collabTAPE</option>
-          </Select>
-        </FormControl>
+        <CustomUpload label="Upload Cover" onChange={handleFileChange} fileName={fileName} acceptFileType="image/*" />
+        <CustomFormInput label="Title" placeholder="what's the title?" value={name} onChange={(e) => setName(e.target.value)} />
+        <CustomFormInput
+          label="Description"
+          placeholder="write a description..."
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          formType="textarea"
+        />
+        <CustomFormInput label="BPM" placeholder="tempo?" value={bpm} onChange={(e) => setBpm(e.target.value)} />
+        <CustomFormInput
+          label="Type of Tape"
+          value={tapeType}
+          onChange={(e) => setTapeType(e.target.value)}
+          formType="select"
+          options={[
+            { value: 'hedstape', label: 'hedsTAPE' },
+            { value: 'collabtape', label: 'collabTAPE' },
+          ]}
+        />
         <Flex justifyContent="flex-end" mt={12}>
-          <Button
-            colorScheme="purple"
-            fontFamily="space"
-            fontWeight="light"
-            letterSpacing="wider"
-            borderRadius="lg"
-            size="sm"
-            rightIcon={<ArrowForwardIcon />}
-            onClick={handleClick}
-            isDisabled={formValidation()}
-          >
-            NEXT
-          </Button>
+          <NextStepButton onClick={handleClick} disabled={formValidation()} text="NEXT" includeIcon />
         </Flex>
       </Stack>
     </Box>
