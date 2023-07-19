62e0fb5f13032544dee6c0d4eb6dc8971da8640c
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-07-19 18:16:21 -0400

    MVP vote function

--- src/pages/vote/screens/Vote.tsx
+++ src/pages/vote/screens/Vote.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { Choice } from 'hedsvote';
 
 // Component
 import { Box, Divider } from '@chakra-ui/react';
