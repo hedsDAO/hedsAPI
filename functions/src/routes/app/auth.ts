110ab0b52990e9db7812f376ddf62c6c4e7bf07b
cgreer011 <camgreer01@gmail.com>
2023-10-02 15:32:10 -0600

    add link user func

--- functions/src/routes/app/auth.ts
+++ functions/src/routes/app/auth.ts
@@ -37,10 +37,10 @@ router.get('/validate-display-name/:displayName', async (req, res) => {
  * @param {express.Request} req - Express request object.
  * @param {express.Response} res - Express response object.
  */
-router.get('/validate-twitter/:twitterHandle', async (req, res) => {
-  const { twitterHandle } = req.params;
+router.get('/validate-twitter/:twitter_handle', async (req, res) => {
+  const { twitter_handle } = req.params;
   try {
-    const validationResult = await validateTwitterHandle(twitterHandle);
+    const validationResult = await validateTwitterHandle(twitter_handle);
     if (validationResult) {
       return res.status(200).json({ validated: true });
     } else {
@@ -131,7 +131,7 @@ router.get("/google-oauth-callback", async (req, res) => {
         return res.json(user);
       } else {
         const createdUser = await createUser({...newUserObject, email});
-        return res.json(createdUser);
+        return res.status(200).json(createdUser);
       }
 
     } else {
@@ -141,5 +141,77 @@ router.get("/google-oauth-callback", async (req, res) => {
      return res.status(400).json({message: 'Token verification failed',error: e});
   }
 })
+
+/**
+ * Link an existing user with an email via Google OAuth or phoneNumber via SMS.
+ *
+ * @route PUT /link/:link-user
+ * @param {express.Request} req - Express request object.
+ * @param {express.Response} res - Express response object.
+ */
+ router.put("/link-user", async (req, res) => {
+  const { link_type, user } = req.body;
+
+  try {
+    if (!user) {
+      return res.status(404).json({ message: "Missing User Data" });
+    }
+
+    if (link_type === "email") {
+      const bearerToken = req.headers.authorization;
+
+      if (!bearerToken || !bearerToken.startsWith('Bearer ')) {
+        throw new Error('Bearer token not found');
+      }
+
+      const token = bearerToken.split('Bearer ')[1];
+      const userInfo = await getGoogleUserData(token);
+
+      if (userInfo) {
+        const email = userInfo['email'];
+
+        const existingUser = await getUserByEmaill(email);
+
+        if (existingUser) {
+          return res.status(400).json({ message: "Email is already associated with another user" });
+        }
+
+        if (user.email) {
+          return res.status(400).json({ message: "User already has an email linked" });
+        }
+
+        user.email = email;
+        const createdUser = await createUser({...newUserObject, email});
+        return res.status(200).json(createdUser);
+
+      } else {
+        throw new Error('Invalid ID token userInfo');
+      }
+
+    } else if (link_type === "phoneNumber") {
+      const { phone_number } = req.body;
+
+      const existingUser = await getUserByPhoneNumber(phone_number);
+
+      if (existingUser) {
+        return res.status(400).json({ message: "Phone number is already associated with another user" });
+      }
+
+      if (user.phone_number) {
+        return res.status(400).json({ message: "User already has a phone number linked" });
+      }
+
+      await sendTwilioVerification(phone_number);
+      return res.status(200).send("Verification code sent.");
+    } else {
+      return res.status(400).json({ message: "Invalid link type" });
+    }
+
+  } catch (error: any) {
+    console.error(error);
+    return res.status(500).send({ message: "Internal server error", error: error.message });
+  }
+});
+
 
 export default router;
