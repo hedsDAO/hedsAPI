4aa227f38996002e1cfb58b85e5d2bbaa600acf7
cgreer011 <camgreer01@gmail.com>
2023-11-22 20:03:53 -0600

    create hedSpace servers

---
+++ functions/src/servers/hedSpace/guestStatus.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import guestStatusRoutes from "../../routes/hedSpace/guestStatus";
+import {onRequest} from "firebase-functions/v2/https";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/", guestStatusRoutes);
+
+export default onRequest({
+  vpcConnector: "heds-app-connector",
+  vpcConnectorEgressSettings: "PRIVATE_RANGES_ONLY",
+  cors: true
+}, app);
