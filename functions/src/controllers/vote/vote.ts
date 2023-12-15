dafacbda9e9491813a9e58b433ca9477562e101b
cgreer011 <camgreer01@gmail.com>
2023-12-14 17:49:50 -0800

    refactor vote in vote

--- functions/src/controllers/vote/vote.ts
+++ functions/src/controllers/vote/vote.ts
@@ -1,4 +1,3 @@
-import { Request, Response } from 'express';
 import { verifyWalletSignature } from '../utils/verifySignature';
 import { determineProposalStatus } from '../utils/determineProposalStatus';
 import { PrismaClient } from '@prisma/client';
@@ -18,99 +17,93 @@ const prisma = new PrismaClient();
  * @async
  * @returns {Promise<Response>} Express response object.
  */
-export async function castVote(req: Request, res: Response) {
-  const newVote = req.body.vote;
-  const voteChoices = req.body.voteChoices;
-  const message: string = req.body.message;
-  const signature: `0x${string}` = req.body.signature;
+export async function castVote(voteBody: any) {
+  const newVote = voteBody.vote;
+  const voteChoices = voteBody.voteChoices;
+  const message: string = voteBody.message;
+  const signature: `0x${string}` = voteBody.signature;
 
   const { vp, voter, proposal_id } = newVote;
 
-  try {
-    const proposal = await prisma.proposals.findUnique({
-      where: { ipfs_hash: proposal_id },
-      select: {
-        start_time: true,
-        end_time: true,
-        is_web3: true,
+  const proposal = await prisma.proposals.findUnique({
+    where: { ipfs_hash: proposal_id },
+    select: {
+      start_time: true,
+      end_time: true,
+      is_web3: true,
+    },
+  });
+
+  if (!proposal) {
+    return 'Proposal not found';
+  }
+
+  if (proposal.is_web3) {
+    const recoveredAddress = await verifyWalletSignature(message, signature);
+    if (recoveredAddress.toLowerCase() !== voter) {
+      return 'Unauthorized: Signature does not match address';
+    }
+  }
+
+  const proposalState = determineProposalStatus(proposal.start_time, proposal.end_time);
+  if (proposalState !== ProposalState.OPEN) {
+    return 'Proposal is not in an open state';
+  }
+
+  const existingVote = await prisma.votes.findFirst({
+    where: {
+      voter: voter,
+      vote_choices: {
+        some: { proposal_id: proposal_id },
+      },
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  let voteId;
+  if (existingVote) {
+    voteId = existingVote.id;
+
+    await prisma.votes.update({
+      where: { id: voteId },
+      data: {
+        signature: signature,
+        created: new Date(),
+        vp: vp,
+        vote_choices: {
+          deleteMany: {
+            vote_id: voteId,
+          },
+        },
       },
     });
-
-    if (!proposal) {
-      return res.status(404).json({ error: 'Proposal not found' });
-    }
-
-    if (proposal.is_web3) {
-      const recoveredAddress = await verifyWalletSignature(message, signature);
-      if (recoveredAddress.toLowerCase() !== voter) {
-        return res.status(403).json({ error: 'Unauthorized: Signature does not match address' });
-      }
-    }
-
-    const proposalState = determineProposalStatus(proposal.start_time, proposal.end_time);
-    if (proposalState !== ProposalState.OPEN) {
-      return res.status(400).json({ error: 'Proposal is not in an open state' });
-    }
-
-    const existingVote = await prisma.votes.findFirst({
-      where: {
+  } else {
+    const createdVote = await prisma.votes.create({
+      data: {
+        signature: signature,
+        created: new Date(),
+        vp: vp,
         voter: voter,
-        vote_choices: {
-          some: { proposal_id: proposal_id },
-        },
       },
       select: {
         id: true,
       },
     });
-
-    let voteId;
-    if (existingVote) {
-      voteId = existingVote.id;
-
-      await prisma.votes.update({
-        where: { id: voteId },
-        data: {
-          signature: signature,
-          created: new Date(),
-          vp: vp,
-          vote_choices: {
-            deleteMany: {
-              vote_id: voteId,
-            },
-          },
-        },
-      });
-    } else {
-      const createdVote = await prisma.votes.create({
-        data: {
-          signature: signature,
-          created: new Date(),
-          vp: vp,
-          voter: voter,
-        },
-        select: {
-          id: true,
-        },
-      });
-      voteId = createdVote.id;
-    }
-
-    for (const voteChoice of voteChoices) {
-      await prisma.vote_choices.create({
-        data: {
-          vote_id: voteId,
-          choice_id: voteChoice.choice_id,
-          proposal_id: voteChoice.proposal_id,
-          amount: voteChoice.amount,
-        },
-      });
-    }
-
-    return res.status(201).json({ voteId, voteChoices });
-  } catch (error: any) {
-    return res.status(500).json({ error: error.message });
-  } finally {
-    await prisma.$disconnect();
+    voteId = createdVote.id;
   }
+
+  for (const voteChoice of voteChoices) {
+    await prisma.vote_choices.create({
+      data: {
+        vote_id: voteId,
+        choice_id: voteChoice.choice_id,
+        proposal_id: voteChoice.proposal_id,
+        amount: voteChoice.amount,
+      },
+    });
+  }
+  await prisma.$disconnect();
+  return { voteId, voteChoices };
 }
--- functions/src/routes/vote/vote.ts
+++ functions/src/routes/vote/vote.ts
@@ -1,8 +1,21 @@
 import * as express from 'express';
+import { toCamelCase } from '../../common';
 import { castVote } from '../../controllers/vote/vote';
 
 const router = express.Router();
 
-router.post('/', castVote);
+router.post('/', async (req, res) => {
+  try {
+    const vote = await castVote(req.body);
+    if (!vote) return res.status(404).json({ error: 'Vote not found' });
+    if (vote === 'Proposal not found') return res.status(404).json({ error: 'Proposal not found' });
+    if (vote === 'Unauthorized: Signature does not match address') return res.status(401).json({ error: 'Unauthorized: Signature does not match address' });
+    if (vote === 'Proposal is not in an open state') return res.status(400).json({ error: 'Proposal is not in an open state' });
+    const convertedVote = toCamelCase(vote);
+    return res.status(200).json(convertedVote);
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 export default router;
