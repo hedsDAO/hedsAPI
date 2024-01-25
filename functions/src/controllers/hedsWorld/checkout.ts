18bc31455b5dac8b5c14b07988bbeca0031f3846
cgreer011 <camgreer01@gmail.com>
2024-01-24 16:59:46 -0800

    old player

--- functions/src/controllers/hedsWorld/checkout.ts
+++ functions/src/controllers/hedsWorld/checkout.ts
@@ -1,17 +1,15 @@
-import { Client, Environment, ApiError, Error } from 'square';
+import { CreatePaymentLinkRequest, Client, Environment, ApiError, Error } from 'square';
 import JSONBig from 'json-bigint';
 import * as functions from 'firebase-functions';
 
-export async function createCheckoutLink(data: any) {
+export async function createCheckoutLink(body: CreatePaymentLinkRequest) {
   try {
     const client = new Client({
       accessToken: process.env.SQUARE_ACCESS_TOKEN,
       environment: Environment.Production,
     });
 
-    const response = await client.checkoutApi.createPaymentLink({
-      ...data,
-    });
+    const response = await client.checkoutApi.createPaymentLink(body);
     return JSONBig.parse(JSONBig.stringify(response.result));
   } catch (error) {
     if (error instanceof ApiError) {
