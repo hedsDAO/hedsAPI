2090c91f4047a9ec5c06d3219e8d26d6bf6cb539
cgreer011 <camgreer01@gmail.com>
2024-03-06 17:19:56 -0800

    remove old songs and users file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import * as functions from "firebase-functions";
-import authRoutes from './routes/app/auth';
-import userRoutes from './routes/users';
-import songRoutes from './routes/songs';
+import authRoutes from "./routes/app/auth";
 
 const app = express();
 app.use(cors({ origin: true }));
@@ -11,13 +9,11 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/auth', authRoutes);
-app.use('/users', userRoutes);
-app.use('/songs', songRoutes);
+app.use("/auth", authRoutes);
 
 export default functions
-.runWith({
-  vpcConnector: "heds-app-connector",
-  vpcConnectorEgressSettings: "PRIVATE_RANGES_ONLY",
-})
-.https.onRequest(app);
+  .runWith({
+    vpcConnector: "heds-app-connector",
+    vpcConnectorEgressSettings: "PRIVATE_RANGES_ONLY",
+  })
+  .https.onRequest(app);
